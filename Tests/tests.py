4dd345353a1f4d70857a03fe0fe67ec8034cd4d2
Alex Goretoy <alex@goretoy.com>
2016-12-31 17:00:39 -0800

    fixed unittests to work with python3 and closed pdftext_file

--- Tests/tests.py
+++ Tests/tests.py
@@ -1,4 +1,5 @@
-import os, sys, unittest
+import os
+import sys
 
 # Configure path environment
 TESTS_ROOT = os.path.abspath(os.path.dirname(__file__))
@@ -14,21 +15,25 @@ from PyPDF2 import PdfFileReader
 
 class PdfReaderTestCases(unittest.TestCase):
     def test_PdfReaderFileLoad(self):
-        '''    Test loading and parsing of a file. Extract text of the file and compare to expected
+        """    Test loading and parsing of a file. Extract text of the file and compare to expected
             textual output. Expected outcome: file loads, text matches expected.
-        '''
+        """
         with open(os.path.join(RESOURCE_ROOT, 'crazyones.pdf'), 'rb') as inputfile:
-            
             # Load PDF file from file
             ipdf = PdfFileReader(inputfile)
             ipdf_p1 = ipdf.getPage(0)
             
             # Retrieve the text of the PDF
-            pdftext_file = open(os.path.join(RESOURCE_ROOT, 'crazyones.txt'), 'r')
+            pdftext_file = open(os.path.join(RESOURCE_ROOT, 'crazyones.txt'), 'rb')
             pdftext = pdftext_file.read()
+            pdftext_file.close()
             ipdf_p1_text = ipdf_p1.extractText()
             
             # Compare the text of the PDF to a known source
-            self.assertEqual(ipdf_p1_text.encode('utf-8', errors='ignore'), pdftext,
+            self.assertEqual(
+                ipdf_p1_text.encode('utf-8', errors='ignore'), 
+                pdftext,
                 msg='PDF extracted text differs from expected value.\n\nExpected:\n\n%r\n\nExtracted:\n\n%r\n\n'
-                    % (pdftext, ipdf_p1_text.encode('utf-8', errors='ignore')))
+                    % (pdftext, ipdf_p1_text.encode('utf-8', errors='ignore'))
+            )
+
