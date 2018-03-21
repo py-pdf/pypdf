4a762b3aed3dccaa5b9a0d481338cae4f3cc27a7
Michel Wilhelm <michel.wilhelm@mercadolivre.com>
2018-03-21 18:36:34 -0300

    Adding missing imports to __all__

--- PyPDF2/__init__.py
+++ PyPDF2/__init__.py
@@ -2,4 +2,9 @@ from .pdf import PdfFileReader, PdfFileWriter
 from .merger import PdfFileMerger
 from .pagerange import PageRange, parse_filename_page_ranges
 from ._version import __version__
-__all__ = ["pdf", "PdfFileMerger"]
+
+
+__all__ = [
+    "pdf", "PdfFileMerger", "PdfFileReader", "PdfFileWriter", 
+    "PageRange", "parse_filename_page_ranges", "__version__"
+]
