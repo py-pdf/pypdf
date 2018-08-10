b423b9e4734a84c963db20c109923e3edb1dc76b
mrstephenneal <stephen.neal14@gmail.com>
2018-08-10 09:43:59 -0400

    Added PdfFileWriter and PdfFileReader to __init.py__

--- PyPDF3/__init__.py
+++ PyPDF3/__init__.py
@@ -2,4 +2,4 @@ from .pdf import PdfFileReader, PdfFileWriter
 from .merger import PdfFileMerger
 from .pagerange import PageRange, parse_filename_page_ranges
 from ._version import __version__
-__all__ = ["pdf", "PdfFileMerger"]
+__all__ = ["pdf", "PdfFileMerger", "PdfFileReader", "PdfFileWriter"]
