8a722b6e7059a9ad4e7c98e82b4470b555c81675
Acsor <nildexo@yandex.com>
2018-08-11 22:35:08 +0200

    Bring source code files up to PEP 8 requirements except for `PyPDF4/`.
    Several style reformattings were added to source code files that were not
    following PEP 8 style conventions. The files residing in `PyPDF4/` were
    excluded from the process.

--- Tests/tests.py
+++ Tests/tests.py
@@ -1,10 +1,9 @@
+import binascii
 import os
 import sys
 import unittest
-import binascii
 
 from PyPDF4 import PdfFileReader, PdfFileWriter
-
 
 # Configure path environment
 TESTS_ROOT = os.path.abspath(os.path.dirname(__file__))
@@ -18,48 +17,64 @@ class PdfReaderTestCases(unittest.TestCase):
 
     def test_PdfReaderFileLoad(self):
         '''
-        Test loading and parsing of a file. Extract text of the file and compare to expected
-        textual output. Expected outcome: file loads, text matches expected.
+        Test loading and parsing of a file. Extract text of the file and
+        compare to expected textual output. Expected outcome: file loads, text
+        matches expected.
         '''
 
-        with open(os.path.join(RESOURCE_ROOT, 'crazyones.pdf'), 'rb') as inputfile:
+        with open(
+                os.path.join(RESOURCE_ROOT, 'crazyones.pdf'), 'rb'
+        ) as inputfile:
             # Load PDF file from file
             ipdf = PdfFileReader(inputfile)
             ipdf_p1 = ipdf.getPage(0)
 
             # Retrieve the text of the PDF
-            with open(os.path.join(RESOURCE_ROOT, 'crazyones.txt'), 'rb') as pdftext_file:
+            with open(
+                    os.path.join(RESOURCE_ROOT, 'crazyones.txt'), 'rb'
+            ) as pdftext_file:
                 pdftext = pdftext_file.read()
 
-            ipdf_p1_text = ipdf_p1.extractText().replace('\n', '').encode('utf-8')
+            ipdf_p1_text = ipdf_p1.extractText(). \
+                replace('\n', '').encode('utf-8')
 
             # Compare the text of the PDF to a known source
-            self.assertEqual(ipdf_p1_text, pdftext,
-                msg='PDF extracted text differs from expected value.\n\nExpected:\n\n%r\n\nExtracted:\n\n%r\n\n'
-                    % (pdftext, ipdf_p1_text))
+            self.assertEqual(
+                ipdf_p1_text, pdftext,
+                msg='PDF extracted text differs from expected value.'
+                    '\n\nExpected:\n\n%r\n\nExtracted:\n\n%r\n\n'
+                    % (pdftext, ipdf_p1_text)
+            )
 
     def test_PdfReaderJpegImage(self):
         '''
-        Test loading and parsing of a file. Extract the image of the file and compare to expected
-        textual output. Expected outcome: file loads, image matches expected.
+        Test loading and parsing of a file. Extract the image of the file and
+        compare to expected textual output. Expected outcome: file loads, image
+        matches expected.
         '''
 
         with open(os.path.join(RESOURCE_ROOT, 'jpeg.pdf'), 'rb') as inputfile:
             # Load PDF file from file
             ipdf = PdfFileReader(inputfile)
-        
+
             # Retrieve the text of the image
-            with open(os.path.join(RESOURCE_ROOT, 'jpeg.txt'), 'r') as pdftext_file:
+            with open(
+                    os.path.join(RESOURCE_ROOT, 'jpeg.txt'), 'r'
+            ) as pdftext_file:
                 imagetext = pdftext_file.read()
-                
-            ipdf_p0 = ipdf.getPage(0)    
+
+            ipdf_p0 = ipdf.getPage(0)
             xObject = ipdf_p0['/Resources']['/XObject'].getObject()
             data = xObject['/Im4'].getData()
-    
+
             # Compare the text of the PDF to a known source
-            self.assertEqual(binascii.hexlify(data).decode(), imagetext, 
-                             msg='PDF extracted image differs from expected value.\n\nExpected:\n\n%r\n\nExtracted:\n\n%r\n\n' 
-                             % (imagetext, binascii.hexlify(data).decode()))
+            self.assertEqual(
+                binascii.hexlify(data).decode(), imagetext,
+                msg='PDF extracted image differs from expected value.'
+                    '\n\nExpected:\n\n%r\n\nExtracted:\n\n%r\n\n'
+                    % (imagetext, binascii.hexlify(data).decode())
+            )
+
 
 class AddJsTestCase(unittest.TestCase):
 
@@ -69,25 +84,47 @@ class AddJsTestCase(unittest.TestCase):
         self.pdf_file_writer.appendPagesFromReader(ipdf)
 
     def test_add(self):
+        self.pdf_file_writer.addJS(
+            "this.print({bUI:true,bSilent:false,bShrinkToFit:true});"
+        )
 
-        self.pdf_file_writer.addJS("this.print({bUI:true,bSilent:false,bShrinkToFit:true});")
-
-        self.assertIn('/Names', self.pdf_file_writer._root_object, "addJS should add a name catalog in the root object.")
-        self.assertIn('/JavaScript', self.pdf_file_writer._root_object['/Names'], "addJS should add a JavaScript name tree under the name catalog.")
-        self.assertIn('/OpenAction', self.pdf_file_writer._root_object, "addJS should add an OpenAction to the catalog.")
+        self.assertIn(
+            '/Names', self.pdf_file_writer._root_object,
+            "addJS should add a name catalog in the root object."
+        )
+        self.assertIn(
+            '/JavaScript', self.pdf_file_writer._root_object['/Names'],
+            "addJS should add a JavaScript name tree under the name catalog."
+        )
+        self.assertIn(
+            '/OpenAction', self.pdf_file_writer._root_object,
+            "addJS should add an OpenAction to the catalog."
+        )
 
     def test_overwrite(self):
-
-        self.pdf_file_writer.addJS("this.print({bUI:true,bSilent:false,bShrinkToFit:true});")
+        self.pdf_file_writer.addJS(
+            "this.print({bUI:true,bSilent:false,bShrinkToFit:true});"
+        )
         first_js = self.get_javascript_name()
 
-        self.pdf_file_writer.addJS("this.print({bUI:true,bSilent:false,bShrinkToFit:true});")
+        self.pdf_file_writer.addJS(
+            "this.print({bUI:true,bSilent:false,bShrinkToFit:true});"
+        )
         second_js = self.get_javascript_name()
 
-        self.assertNotEqual(first_js, second_js, "addJS should overwrite the previous script in the catalog.")
+        self.assertNotEqual(
+            first_js, second_js,
+            "addJS should overwrite the previous script in the catalog."
+        )
 
     def get_javascript_name(self):
         self.assertIn('/Names', self.pdf_file_writer._root_object)
-        self.assertIn('/JavaScript', self.pdf_file_writer._root_object['/Names'])
-        self.assertIn('/Names', self.pdf_file_writer._root_object['/Names']['/JavaScript'])
-        return self.pdf_file_writer._root_object['/Names']['/JavaScript']['/Names'][0]
+        self.assertIn(
+            '/JavaScript', self.pdf_file_writer._root_object['/Names']
+        )
+        self.assertIn(
+            '/Names',
+            self.pdf_file_writer._root_object['/Names']['/JavaScript']
+        )
+        return self.pdf_file_writer. \
+            _root_object['/Names']['/JavaScript']['/Names'][0]
