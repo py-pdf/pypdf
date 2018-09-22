0c9ceb65707f65814291999a60f4fc60b5bae97e
Acsor <nildexo@yandex.com>
2018-09-22 11:25:21 +0200

    Improve `__all__` in `pypdf/__init__.py`.

--- pypdf/__init__.py
+++ pypdf/__init__.py
@@ -3,4 +3,10 @@ from pypdf.merger import PdfFileMerger
 from pypdf.pagerange import PageRange
 from pypdf._version import __version__
 
-__all__ = ["PdfFileReader", "PdfFileWriter", "PdfFileMerger", "PageRange"]
+
+__all__ = [
+    # Basic PyPDF elements
+    "PdfFileReader", "PdfFileWriter", "PdfFileMerger", "PageRange",
+    # PyPDF modules
+    "pdf", "generic", "utils", "filters", "merger", "pagerange", "xmp"
+]
