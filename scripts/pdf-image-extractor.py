21c627f0c38ab9065e561a7efe8d66d5fa84bd84
Acsor <nildexo@yandex.com>
2018-09-26 18:09:21 +0200

    Slightly adjust `scripts/pdf-image-extractor.py` (but it is kept not fully working still).

--- scripts/pdf-image-extractor.py
+++ scripts/pdf-image-extractor.py
@@ -1,5 +1,5 @@
 """
-Extract images from PDF without resampling or altering.
+Extract images from PDFs without resampling or altering.
 
 Adapted from work by Sylvain Pelissier
 http://stackoverflow.com/questions/2693820/extract-images-from-pdf-without-resampling-in-python
@@ -13,53 +13,62 @@ from PIL import Image
 from os.path import abspath, dirname, join
 
 PROJECT_ROOT = abspath(
-    join(dirname(__file__), os.pardir, os.pardir)
+    join(dirname(__file__), os.pardir)
 )
 sys.path.append(PROJECT_ROOT)
 
-import pypdf
+from pypdf import PdfFileReader
 
 
-if len(sys.argv) != 2:
-    print("\nUsage: python {} input_file\n".format(sys.argv[0]))
-    sys.exit(1)
+def main():
+    if len(sys.argv) != 2:
+        print("{}: <filepath>".format(sys.argv[0]))
+        return 1
 
-pdf = sys.argv[1]
+    filepath = sys.argv[1].strip()
+    r = PdfFileReader(open(filepath, "rb"))
+    pageNo = 0
 
-if __name__ == '__main__':
-    input1 = pypdf.PdfFileReader(open(pdf, "rb"))
-    page0 = input1.getPage(30)
+    while (pageNo < r.numPages):
+        page = r.getPage(pageNo)
 
-    if '/XObject' in page0['/Resources']:
-        xObject = page0['/Resources']['/XObject'].getObject()
+        if '/XObject' in page['/Resources']:
+            xObject = page['/Resources']['/XObject'].getObject()
 
-        for obj in xObject:
-            if xObject[obj]['/Subtype'] == '/Image':
-                size = (xObject[obj]['/Width'], xObject[obj]['/Height'])
-                data = xObject[obj].getData()
-                if xObject[obj]['/ColorSpace'] == '/DeviceRGB':
-                    mode = "RGB"
-                else:
-                    mode = "P"
-                
-                if '/Filter' in xObject[obj]:
-                    if xObject[obj]['/Filter'] == '/FlateDecode':
+            for obj in xObject:
+                if xObject[obj]['/Subtype'] == '/Image':
+                    size = (xObject[obj]['/Width'], xObject[obj]['/Height'])
+                    data = xObject[obj].getData()
+
+                    if xObject[obj]['/ColorSpace'] == '/DeviceRGB':
+                        mode = "RGB"
+                    else:
+                        mode = "P"
+
+                    if '/Filter' in xObject[obj]:
+                        if xObject[obj]['/Filter'] == '/FlateDecode':
+                            img = Image.frombytes(mode, size, data)
+                            img.save(obj[1:] + ".png")
+                        elif xObject[obj]['/Filter'] == '/DCTDecode':
+                            img = open(obj[1:] + ".jpg", "wb")
+                            img.write(data)
+                            img.close()
+                        elif xObject[obj]['/Filter'] == '/JPXDecode':
+                            img = open(obj[1:] + ".jp2", "wb")
+                            img.write(data)
+                            img.close()
+                        elif xObject[obj]['/Filter'] == '/CCITTFaxDecode':
+                            img = open(obj[1:] + ".tiff", "wb")
+                            img.write(data)
+                            img.close()
+                    else:
                         img = Image.frombytes(mode, size, data)
                         img.save(obj[1:] + ".png")
-                    elif xObject[obj]['/Filter'] == '/DCTDecode':
-                        img = open(obj[1:] + ".jpg", "wb")
-                        img.write(data)
-                        img.close()
-                    elif xObject[obj]['/Filter'] == '/JPXDecode':
-                        img = open(obj[1:] + ".jp2", "wb")
-                        img.write(data)
-                        img.close()
-                    elif xObject[obj]['/Filter'] == '/CCITTFaxDecode':
-                        img = open(obj[1:] + ".tiff", "wb")
-                        img.write(data)
-                        img.close()
-                else:
-                    img = Image.frombytes(mode, size, data)
-                    img.save(obj[1:] + ".png")
-    else:
-        print("No image found.")
+        else:
+            print("No image found.")
+
+        pageNo += 1
+
+
+if __name__ == '__main__':
+    sys.exit(main())
