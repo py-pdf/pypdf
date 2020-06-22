7e1ed3c5da6919509b1cc2c7a94a8b4d5f945d93
claird <claird@opentext.com>
2020-06-22 10:24:41 -0500

    Boost Pylint conformance.

--- tests/test_generic.py
+++ tests/test_generic.py
@@ -20,65 +20,67 @@ sys.path.append(PROJECT_ROOT)
 
 
 class ObjectStreamTestCase(unittest.TestCase):
-    def testObjectIds(self):
+    """ [EXPLAIN THIS.] """
+    def test_object_ids(self):
         """
         Tests the ``ObjectStream.objectIds()`` method.
         """
-        expResults = (
+        exp_results = (
             (8, 3, 10, 2, 1, 11, 13, 15, 4, 19, 5, 20, 6, 21, 17),
             (644, 642, 646, 647, 648, 122, 119, 120, 121, 124, 179, 232, 327,
              467, 478, 519, 568, 573, 580, 586, 592, 598, 603, 611, 616, 623,
              629, 634),
         )
         # Files we know to have Object Streams within
-        inputData = (
+        input_data = (
             # (filename, id, generation number)
             ("crazyones.pdf", 9, 0),
             ("GeoBase_NHNC1_Data_Model_UML_EN.pdf", 645, 0),
         )
 
-        for o, d in zip(expResults, inputData):
-            filepath = join(TESTS_DATA_ROOT, d[0])
-            r = PdfFileReader(filepath)
-            ref = IndirectObject(d[1], d[2], r)
-            objStm = r.getObject(ref)
+        for o__, d__ in zip(exp_results, input_data):
+            filepath = join(TESTS_DATA_ROOT, d__[0])
+            r__ = PdfFileReader(filepath)
+            ref = IndirectObject(d__[1], d__[2], r__)
+            obj_stm = r__.getObject(ref)
 
-            r.close()
+            r__.close()
 
-            self.assertIsInstance(objStm, ObjectStream)
-            self.assertTupleEqual(tuple(o), tuple(objStm.objectIds))
+            self.assertIsInstance(obj_stm, ObjectStream)
+            self.assertTupleEqual(tuple(o__), tuple(obj_stm.objectIds))
 
 class TextStringObjectTestCase(unittest.TestCase):
+    """ [EXPLAIN THIS.] """
     @staticmethod
-    def _getOutputBytesForString(inputString):
+    def _get_output_bytes_for_string(input_string):
         stream = io.BytesIO()
-        textStringObject = TextStringObject(inputString)
-        textStringObject.writeToStream(stream, encryption_key=None)
-        streamOutput = stream.getvalue()
-        return streamOutput
+        text_string_object = TextStringObject(input_string)
+        text_string_object.writeToStream(stream, encryption_key=None)
+        stream_output = stream.getvalue()
+        return stream_output
 
-    def testWriteToStream(self):
+    def test_write_to_stream(self):
         """
         Tests the ``TextStringObject.writeToStream()`` method.
         """
 
-        outputForLowercaseLetter = self._getOutputBytesForString('k')
-        self.assertEqual(outputForLowercaseLetter, b'(k)')
+        output_for_lowercase_letter = self._get_output_bytes_for_string('k')
+        self.assertEqual(output_for_lowercase_letter, b'(k)')
 
-        outputForUppercaseLetter = self._getOutputBytesForString('K')
-        self.assertEqual(outputForUppercaseLetter, b'(K)')
+        output_for_uppercase_letter = self._get_output_bytes_for_string('K')
+        self.assertEqual(output_for_uppercase_letter, b'(K)')
 
-        outputForDigit = self._getOutputBytesForString('7')
-        self.assertEqual(outputForDigit, b'(7)')
+        output_for_digit = self._get_output_bytes_for_string('7')
+        self.assertEqual(output_for_digit, b'(7)')
 
-        outputForSpace = self._getOutputBytesForString(' ')
-        self.assertEqual(outputForSpace, b'( )')
+        output_for_space = self._get_output_bytes_for_string(' ')
+        self.assertEqual(output_for_space, b'( )')
 
-        outputForOpeningParentheses = self._getOutputBytesForString('(')
-        self.assertEqual(outputForOpeningParentheses, b'(\\050)')
+        output_for_opening_parentheses = self._get_output_bytes_for_string('(')
+        self.assertEqual(output_for_opening_parentheses, b'(\\050)')
 
-        outputForBackslash = self._getOutputBytesForString('\\')
-        self.assertEqual(outputForBackslash, b'(\\134)')
+        output_for_backslash = self._get_output_bytes_for_string('\\')
+        self.assertEqual(output_for_backslash, b'(\\134)')
 
 
 if __name__ == "__main__":
--- tests/utils.py
+++ tests/utils.py
@@ -4,29 +4,29 @@ internal project codebase.
 """
 
 
-def intToBitstring(n, fill=8):
+def intToBitstring(n__, fill=8):
     """
     Turns an integer ``n`` into its corresponding textual bit representation.
 
     :param fill: number of zeros to pad the bit representation with.
     :raises TypeError: if n is not an integer.
     """
-    if not isinstance(n, int):
+    if not isinstance(n__, int):
         raise TypeError("n must be an integer")
 
-    return ("{bits:0>%db}" % fill).format(bits=n)
+    return ("{bits:0>%db}" % fill).format(bits=n__)
 
 
-def bitstringToInt(b):
+def bitstringToInt(b__):
     """Performs the reverse of ``intToBitstring()``."""
-    if not isinstance(b, str):
-        raise TypeError("Expected str, got %s" % b.__class__)
-    if not set(b).issubset({"0", "1"}):
+    if not isinstance(b__, str):
+        raise TypeError("Expected str, got %s" % b__.__class__)
+    if not set(b__).issubset({"0", "1"}):
         raise ValueError("b must be a string containing only 0's and 1's")
 
-    result, bitlen = 0, len(b)
+    result, bitlen = 0, len(b__)
 
-    for index, i in enumerate(b):
+    for index, i in enumerate(b__):
         if i == "1":
             result += 2 ** (bitlen - index - 1)
 
