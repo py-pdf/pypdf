89969b988cd18b6371e8296513b48ec76bc68be6
claird <claird@opentext.com>
2020-06-21 19:03:33 -0500

    Update the safe parts of pull request #75.

--- pypdf/__init__.py
+++ pypdf/__init__.py
@@ -1,12 +1,16 @@
-from pypdf.pdf import PdfFileReader, PdfFileWriter
-from pypdf.merger import PdfFileMerger
-from pypdf.pagerange import PageRange
-from pypdf._version import __version__
+from .pdf import PdfFileReader, PdfFileWriter
+from .generic import *
+from .merger import PdfFileMerger
+from .pagerange import PageRange
+from ._version import __version__
 
 
 __all__ = [
     # Basic PyPDF elements
     "PdfFileReader", "PdfFileWriter", "PdfFileMerger", "PageRange",
+    # most used elements from generic
+    "BooleanObject","ArrayObject","IndirectObject","FloatObject","NumberObject","createStringObject",
+    "TextStringObject","NameObject","DictionaryObject","TreeObject","Destination","PageLabel","Bookmark",
     # PyPDF modules
     "pdf", "generic", "utils", "filters", "merger", "pagerange", "xmp"
 ]
