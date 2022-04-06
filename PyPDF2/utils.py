b17db7994abb3611c2aa660bc45d3607dbaf9560
Korbinian Preisler <timitos@virtual-things.biz>
2022-04-06 07:37:13 +0200

    BUG: Fix formatWarning for filename without slash (#612)

--- PyPDF2/utils.py
+++ PyPDF2/utils.py
@@ -66,7 +66,7 @@ def isBytes(b):
 
 #custom implementation of warnings.formatwarning
 def formatWarning(message, category, filename, lineno, line=None):
-    file = filename.replace("/", "\\").rsplit("\\", 1)[1] # find the file name
+    file = filename.replace("/", "\\").rsplit("\\", 1)[-1] # find the file name
     return "%s: %s [%s:%s]\n" % (category.__name__, message, file, lineno)
 
 
