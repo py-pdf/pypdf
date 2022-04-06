807bb0b521ece12bd33147088288f96cf1d5aa17
Martin Thoma <info@martin-thoma.de>
2022-04-06 19:57:49 +0200

    TST: Add test for XMP module

---
+++ Tests/test_xmp.py
@@ -0,0 +1,20 @@
+import os
+import pytest
+import PyPDF2
+
+TESTS_ROOT = os.path.abspath(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.dirname(TESTS_ROOT)
+RESOURCE_ROOT = os.path.join(PROJECT_ROOT, "Resources")
+
+@pytest.mark.parametrize(
+    "src,has_xmp",
+    [
+        (os.path.join(RESOURCE_ROOT, "commented-xmp.pdf"), True),
+        (os.path.join(RESOURCE_ROOT, "crazyones.pdf"), False),
+    ],
+)
+def test_read_xmp(src, has_xmp):
+    with open(src, "rb") as inputfile:
+        ipdf = PyPDF2.PdfFileReader(inputfile)
+        xmp = ipdf.getXmpMetadata()
+        assert (xmp is None) == (not has_xmp)
