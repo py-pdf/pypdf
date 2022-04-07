c25814a86ebc65a840cdfca512da17d9d115de07
Martin Thoma <info@martin-thoma.de>
2022-04-07 20:07:22 +0200

    DOC: Use README on PyPI
    Also move package metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -3,24 +3,8 @@
 from setuptools import setup
 import re
 
-long_description = """
-A Pure-Python library built as a PDF toolkit.  It is capable of:
 
-- extracting document information (title, author, ...)
-- splitting documents page by page
-- merging documents page by page
-- cropping pages
-- merging multiple pages into a single page
-- encrypting and decrypting PDF files
-- and more!
-
-By being Pure-Python, it should run on any Python platform without any
-dependencies on external libraries.  It can also work entirely on StringIO
-objects rather than file streams, allowing for PDF manipulation in memory.
-It is therefore a useful tool for websites that manage or manipulate PDFs.
-"""
-
-VERSIONFILE="PyPDF2/_version.py"
+VERSIONFILE = "PyPDF2/_version.py"
 verstrline = open(VERSIONFILE, "rt").read()
 VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
 mo = re.search(VSRE, verstrline, re.M)
@@ -29,29 +13,4 @@ if mo:
 else:
     raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE))
 
-setup(
-        name="PyPDF2",
-        version=verstr,
-        description="PDF toolkit",
-        long_description=long_description,
-        author="Mathieu Fenniak",
-        author_email="biziqe@mathieu.fenniak.net",
-        maintainer="Phaseit, Inc.",
-        maintainer_email="PyPDF2@phaseit.net",
-        url="https://mstamy2.github.io/PyPDF2",
-        project_urls={
-            "Source": "https://github.com/mstamy2/PyPDF2",
-            "Bug Reports": "https://github.com/mstamy2/PyPDF2/issues",
-            "Changelog": "https://raw.githubusercontent.com/mstamy2/PyPDF2/master/CHANGELOG",
-        },
-        classifiers = [
-            "Development Status :: 5 - Production/Stable",
-            "Intended Audience :: Developers",
-            "License :: OSI Approved :: BSD License",
-            "Programming Language :: Python :: 2",
-            "Programming Language :: Python :: 3",
-            "Operating System :: OS Independent",
-            "Topic :: Software Development :: Libraries :: Python Modules",
-            ],
-        packages=["PyPDF2"],
-    )
+setup(version=verstr)
