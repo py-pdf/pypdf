93a6063dfa93b5439b95d3a39f4e9bb4892f77f2
Martin Thoma <info@martin-thoma.de>
2022-04-10 18:35:11 +0200

    STY: Apply black

--- Scripts/booklet.py
+++ Scripts/booklet.py
@@ -16,13 +16,13 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('input', type=argparse.FileType('rb'))
-    parser.add_argument('output')
+    parser.add_argument("input", type=argparse.FileType("rb"))
+    parser.add_argument("output")
     args = parser.parse_args()
 
     reader = PyPDF2.PdfFileReader(args.input)
     numPages = reader.getNumPages()
-    print('Pages in file:', numPages)
+    print("Pages in file:", numPages)
 
     pagesPerSheet = 4
     virtualPages = (numPages + pagesPerSheet - 1) // pagesPerSheet * pagesPerSheet
@@ -31,16 +31,19 @@ def main():
     mb = firstPage.mediaBox
     pageWidth = 2 * mb.getWidth()
     pageHeight = mb.getHeight()
-    print('Medium size:', '{}x{}'.format(pageWidth, pageHeight))
+    print("Medium size:", "{}x{}".format(pageWidth, pageHeight))
 
     writer = PyPDF2.PdfFileWriter()
 
     def scale(page):
-        return min(mb.getWidth() / page.mediaBox.getWidth(), mb.getHeight() / page.mediaBox.getHeight())
+        return min(
+            mb.getWidth() / page.mediaBox.getWidth(),
+            mb.getHeight() / page.mediaBox.getHeight(),
+        )
 
     def mergePage(dst, src, xOffset):
         pageScale = scale(src)
-        print('scaling by', pageScale)
+        print("scaling by", pageScale)
         dx = (mb.getWidth() - pageScale * src.mediaBox.getWidth()) / 2
         dy = (mb.getHeight() - pageScale * src.mediaBox.getHeight()) / 2
         dst.mergeScaledTranslatedPage(src, scale(src), xOffset + dx, dy)
@@ -48,7 +51,7 @@ def main():
     def mergePageByNumber(dstPage, pageNumber, xOffset):
         if pageNumber >= numPages:
             return
-        print('merging page', pageNumber, 'with offset', xOffset)
+        print("merging page", pageNumber, "with offset", xOffset)
         page = reader.getPage(pageNumber)
         mergePage(dstPage, page, xOffset)
 
@@ -60,8 +63,8 @@ def main():
         mergePageByNumber(page, i, offsets[0])
         mergePageByNumber(page, virtualPages - i - 1, offsets[1])
 
-    writer.write(open(args.output, 'wb'))
+    writer.write(open(args.output, "wb"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- Tests/test_reader.py
+++ Tests/test_reader.py
@@ -122,32 +122,35 @@ def test_get_images(src, nb_images):
     ],
 )
 def test_get_images_raw(strict, with_prev_0, should_fail):
-    pdf_data = b"%%PDF-1.7\n" \
-               b"1 0 obj << /Count 1 /Kids [4 0 R] /Type /Pages >> endobj\n" \
-               b"2 0 obj << >> endobj\n" \
-               b"3 0 obj << >> endobj\n" \
-               b"4 0 obj << /Contents 3 0 R /CropBox [0.0 0.0 2550.0 3508.0]" \
-               b" /MediaBox [0.0 0.0 2550.0 3508.0] /Parent 1 0 R" \
-               b" /Resources << /Font << >> >>" \
-               b" /Rotate 0 /Type /Page >> endobj\n" \
-               b"5 0 obj << /Pages 1 0 R /Type /Catalog >> endobj\n" \
-               b"xref 1 5\n" \
-               b"%010d 00000 n\n" \
-               b"%010d 00000 n\n" \
-               b"%010d 00000 n\n" \
-               b"%010d 00000 n\n" \
-               b"%010d 00000 n\n" \
-               b"trailer << %s/Root 5 0 R /Size 6 >>\n" \
-               b"startxref %d\n" \
-               b"%%%%EOF"
-    pdf_data = pdf_data % (pdf_data.find(b"1 0 obj"),
-                           pdf_data.find(b"2 0 obj"),
-                           pdf_data.find(b"3 0 obj"),
-                           pdf_data.find(b"4 0 obj"),
-                           pdf_data.find(b"5 0 obj"),
-                           b"/Prev 0 " if with_prev_0 else b"",
-                           pdf_data.find(b"xref"),
-                           )
+    pdf_data = (
+        b"%%PDF-1.7\n"
+        b"1 0 obj << /Count 1 /Kids [4 0 R] /Type /Pages >> endobj\n"
+        b"2 0 obj << >> endobj\n"
+        b"3 0 obj << >> endobj\n"
+        b"4 0 obj << /Contents 3 0 R /CropBox [0.0 0.0 2550.0 3508.0]"
+        b" /MediaBox [0.0 0.0 2550.0 3508.0] /Parent 1 0 R"
+        b" /Resources << /Font << >> >>"
+        b" /Rotate 0 /Type /Page >> endobj\n"
+        b"5 0 obj << /Pages 1 0 R /Type /Catalog >> endobj\n"
+        b"xref 1 5\n"
+        b"%010d 00000 n\n"
+        b"%010d 00000 n\n"
+        b"%010d 00000 n\n"
+        b"%010d 00000 n\n"
+        b"%010d 00000 n\n"
+        b"trailer << %s/Root 5 0 R /Size 6 >>\n"
+        b"startxref %d\n"
+        b"%%%%EOF"
+    )
+    pdf_data = pdf_data % (
+        pdf_data.find(b"1 0 obj"),
+        pdf_data.find(b"2 0 obj"),
+        pdf_data.find(b"3 0 obj"),
+        pdf_data.find(b"4 0 obj"),
+        pdf_data.find(b"5 0 obj"),
+        b"/Prev 0 " if with_prev_0 else b"",
+        pdf_data.find(b"xref"),
+    )
     pdf_stream = io.BytesIO(pdf_data)
     if should_fail:
         with pytest.raises(PyPDF2.pdf.utils.PdfReadError):
