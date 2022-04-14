6399908880ce428e5af432c5c0783eae670f2f93
clach04 <clach04@gmail.com>
2022-04-13 20:49:45 -0700

    Fix issues #511 - title sometimes none

--- PyPDF2/pdf.py
+++ PyPDF2/pdf.py
@@ -2903,7 +2903,7 @@ class DocumentInformation(DictionaryObject):
             return retval
         return None
 
-    title = property(lambda self: self.getText("/Title"))
+    title = property(lambda self: self.getText("/Title") or self['/Title'])
     """Read-only property accessing the document's **title**.
     Returns a unicode string (``TextStringObject``) or ``None``
     if the title is not specified."""
