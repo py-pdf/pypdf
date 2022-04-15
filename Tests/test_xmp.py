f2d8ac87697639e09c9d3302f598b054c214e9ca
Martin Thoma <info@martin-thoma.de>
2022-04-15 09:57:08 +0200

    Fix Py2

--- Tests/test_xmp.py
+++ Tests/test_xmp.py
@@ -23,7 +23,7 @@ def test_read_xmp(src, has_xmp):
         for el in xmp.getElement(
             aboutUri="", namespace=PyPDF2.xmp.RDF_NAMESPACE, name="Artist"
         ):
-            print(f"el={el}")
+            print("el={el}".format(el=el))
 
         assert get_all_tiff(xmp) == {"tiff:Artist": ["me"]}
         assert xmp.dc_contributor == []
