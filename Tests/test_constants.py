845c8e8298292947b6c1e05bb31e2620467c6d63
Martin Thoma <info@martin-thoma.de>
2022-04-15 22:46:01 +0200

    Make tests more rigid

--- Tests/test_constants.py
+++ Tests/test_constants.py
@@ -1,9 +1,14 @@
+import re
+
 from PyPDF2.constants import PDF_KEYS
 
 
 def test_slash_prefix():
+    pattern = re.compile(r"^\/[A-Z]+[a-zA-Z0-9]*$")
     for cls in PDF_KEYS:
         for attr in dir(cls):
             if attr.startswith("__") and attr.endswith("__"):
                 continue
-            assert getattr(cls, attr).startswith("/")
+            constant_value = getattr(cls, attr)
+            assert constant_value.startswith("/")
+            assert pattern.match(constant_value)
