8d3241be7a92dc1da21cdf5d483e291001a26590
Martin Thoma <info@martin-thoma.de>
2022-04-16 23:43:34 +0200

    TST: Run tests in CI once with -OO Flags (#770)

--- PyPDF2/pdf.py
+++ PyPDF2/pdf.py
@@ -2287,7 +2287,8 @@ class PageObject(DictionaryObject):
         :param int angle: Angle to rotate the page.  Must be an increment
             of 90 deg.
         """
-        assert angle % 90 == 0
+        if angle % 90 != 0:
+            raise ValueError("Rotation angle must be a multiple of 90")
         self._rotate(angle)
         return self
 
@@ -2298,7 +2299,8 @@ class PageObject(DictionaryObject):
         :param int angle: Angle to rotate the page.  Must be an increment
             of 90 deg.
         """
-        assert angle % 90 == 0
+        if angle % 90 != 0:
+            raise ValueError("Rotation angle must be a multiple of 90")
         self._rotate(-angle)
         return self
 
