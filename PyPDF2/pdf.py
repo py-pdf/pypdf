03ea3ec6ef8cfb57724ba2b47c48cf6e2d975856
Rafael <rltpoa@users.noreply.github.com>
2022-04-16 07:34:45 -0300

    BUG: Fix reading more than last1K for EOF (#642)
    Added optional parameter in readNextEndLine() to limit the offset
    then read() uses this parameter to limit the reading to last1K
    
    Closes #639
    Closes #439

--- PyPDF2/pdf.py
+++ PyPDF2/pdf.py
@@ -1778,7 +1778,7 @@ class PdfFileReader(object):
         while line[:5] != b_("%%EOF"):
             if stream.tell() < last1K:
                 raise utils.PdfReadError("EOF marker not found")
-            line = self.readNextEndLine(stream)
+            line = self.readNextEndLine(stream, last1K)
             if debug: print("  line:",line)
 
         # find startxref entry - the location of the xref table
@@ -2020,13 +2020,13 @@ class PdfFileReader(object):
             if (i+1) >= len(array):
                 break
 
-    def readNextEndLine(self, stream):
+    def readNextEndLine(self, stream, limit_offset=0):
         debug = False
         if debug: print(">>readNextEndLine")
         line = b_("")
         while True:
             # Prevent infinite loops in malformed PDFs
-            if stream.tell() == 0:
+            if stream.tell() == 0 or stream.tell() == limit_offset:
                 raise utils.PdfReadError("Could not read malformed PDF file")
             x = stream.read(1)
             if debug: print(("  x:", x, "%x"%ord(x)))
