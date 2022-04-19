9092b4bcb53eaeb22134e5250d92446d279505f6
github-action-benchmark <github@users.noreply.github.com>
2022-04-19 12:41:29 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 3dac50386da3c6d310d4a9ece804bc140e8e0798

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1650371124928,
+  "lastUpdate": 1650372089203,
   "repoUrl": "https://github.com/py-pdf/PyPDF2",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -27,6 +27,40 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.004305996984652197",
             "extra": "mean: 1.0724763806 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "py-pdf",
+            "username": "py-pdf"
+          },
+          "committer": {
+            "name": "py-pdf",
+            "username": "py-pdf"
+          },
+          "id": "3dac50386da3c6d310d4a9ece804bc140e8e0798",
+          "message": "Add Benchmark for Performance Testing",
+          "timestamp": "2022-04-19T10:36:47Z",
+          "url": "https://github.com/py-pdf/PyPDF2/pull/781/commits/3dac50386da3c6d310d4a9ece804bc140e8e0798"
+        },
+        "date": 1650372088110,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "Tests/bench.py::test_page_operations",
+            "value": 0.9422550305909239,
+            "unit": "iter/sec",
+            "range": "stddev: 0.006487516920717905",
+            "extra": "mean: 1.0612838005999947 sec\nrounds: 5"
+          },
+          {
+            "name": "Tests/bench.py::test_merge",
+            "value": 10.256656597871526,
+            "unit": "iter/sec",
+            "range": "stddev: 0.004297642398638799",
+            "extra": "mean: 97.49765827272809 msec\nrounds: 11"
           }
         ]
       }
