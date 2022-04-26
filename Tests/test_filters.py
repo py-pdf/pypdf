96d8d0f937dbffc867f0d49d66f5c40b36bb431d
Martin Thoma <info@martin-thoma.de>
2022-04-26 18:20:09 +0200

    TST: Add Test for ASCII85Decode (#825)
    Full credit to
    https://github.com/py-pdf/PyPDF2/pull/817/commits/6dc90b1e64c1965b61aa2561ce912f64ca19cad4
    
    Co-authored-by: Acsor <nildexo@yandex.com>

--- Tests/test_filters.py
+++ Tests/test_filters.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
 import string
 from itertools import product as cartesian_product
 
 import pytest
 
 from PyPDF2.errors import PdfReadError, PdfStreamError
-from PyPDF2.filters import ASCIIHexDecode, FlateDecode
+from PyPDF2.filters import ASCIIHexDecode, FlateDecode, ASCII85Decode
 
 filter_inputs = (
     # "", '', """""",
@@ -97,3 +98,43 @@ def test_ASCIIHexDecode_no_eod():
     with pytest.raises(PdfStreamError) as exc:
         ASCIIHexDecode.decode("")
     assert exc.value.args[0] == "Unexpected EOD in ASCIIHexDecode"
+
+
+@pytest.mark.xfail
+def test_ASCII85Decode_with_overflow():
+    inputs = (
+        v + "~>"
+        for v in "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x0e\x0f"
+        "\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a"
+        "\x1b\x1c\x1d\x1e\x1fvwxy{|}~\x7f\x80\x81\x82"
+        "\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d"
+        "\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98"
+        "\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0¡¢£¤¥¦§¨©ª«¬"
+        "\xad®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇ"
+    )
+
+    for i in inputs:
+        with pytest.raises(ValueError) as exc:
+            ASCII85Decode.decode(i)
+        assert exc.value.args[0] == ""
+
+
+@pytest.mark.no_py27
+def test_ASCII85Decode_five_zero_bytes():
+    """
+    From ISO 32000 (2008) §7.4.3:
+    «As a special case, if all five bytes are 0, they shall be represented
+    by the character with code 122 (z) instead of by five exclamation
+    points (!!!!!).»
+    """
+    inputs = ("z", "zz", "zzz")
+    exp_outputs = (
+        b"\x00\x00\x00\x00",
+        b"\x00\x00\x00\x00" * 2,
+        b"\x00\x00\x00\x00" * 3,
+    )
+
+    assert ASCII85Decode.decode("!!!!!") == ASCII85Decode.decode("z")
+
+    for expected, i in zip(exp_outputs, inputs):
+        assert ASCII85Decode.decode(i) == expected
