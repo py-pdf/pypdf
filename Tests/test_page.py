1200b68941f9dcb9701029c353901953d1f540f3
Martin Thoma <info@martin-thoma.de>
2022-04-26 15:38:01 +0200

    Flake8 fix

--- Tests/test_page.py
+++ Tests/test_page.py
@@ -10,12 +10,12 @@ PROJECT_ROOT = os.path.dirname(TESTS_ROOT)
 RESOURCE_ROOT = os.path.join(PROJECT_ROOT, "Resources")
 EXTERNAL_ROOT = os.path.join(PROJECT_ROOT, "sample-files")
 
+
 def get_all_sample_files():
     with open(os.path.join(EXTERNAL_ROOT, "files.json")) as fp:
         data = fp.read()
     meta = json.loads(data)
     return meta
-    
 
 
 all_files_meta = get_all_sample_files()
@@ -24,14 +24,14 @@ all_files_meta = get_all_sample_files()
 @pytest.mark.external
 @pytest.mark.parametrize(
     "meta",
-    [m for m in all_files_meta["data"] if not m['encrypted']],
-    ids=[m['path'] for m in all_files_meta["data"] if not m['encrypted']],
+    [m for m in all_files_meta["data"] if not m["encrypted"]],
+    ids=[m["path"] for m in all_files_meta["data"] if not m["encrypted"]],
 )
 def test_read(meta):
-    pdf_path = os.path.join(EXTERNAL_ROOT, meta['path'])
+    pdf_path = os.path.join(EXTERNAL_ROOT, meta["path"])
     reader = PdfFileReader(pdf_path)
     reader.pages[0]
-    assert len(reader.pages) == meta['pages']
+    assert len(reader.pages) == meta["pages"]
 
 
 @pytest.mark.parametrize(
