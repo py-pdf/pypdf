04c36c2df602d2e8c578ef88d734ac80f1ec0aa5
Martin Thoma <info@martin-thoma.de>
2022-05-08 10:33:16 +0200

    MAINT: merger.py ➔ _merger.py

--- PyPDF2/__init__.py
+++ PyPDF2/__init__.py
@@ -1,7 +1,7 @@
+from PyPDF2._merger import PdfFileMerger
 from PyPDF2._reader import PdfFileReader
 from PyPDF2._version import __version__
 from PyPDF2._writer import PdfFileWriter
-from PyPDF2.merger import PdfFileMerger
 from PyPDF2.pagerange import PageRange, parse_filename_page_ranges
 from PyPDF2.papersizes import PaperSize
 
