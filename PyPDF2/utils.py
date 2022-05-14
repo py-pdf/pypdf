af6066aa6337790fee13b641fd11fecfeb126f59
Martin Thoma <info@martin-thoma.de>
2022-05-15 00:14:34 +0200

    Change deprecation warnings

--- PyPDF2/utils.py
+++ PyPDF2/utils.py
@@ -2,6 +2,8 @@ from PyPDF2._security import RC4_encrypt  # noqa: F401
 from PyPDF2._utils import *
 
 warnings.warn(
-    "`PyPDF2.utils` will be removed with PyPDF2 2.0.0",
+    "The PyPDF2.utils module is deprecated. "
+    "Import either from PyPDF2 directly, PyPDF2.errors, or PyPDF2.generic",
     PendingDeprecationWarning,
+    stacklevel=2,
 )
