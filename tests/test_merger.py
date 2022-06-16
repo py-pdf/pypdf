1ccf4849f0933b34d6f1e1d8f1c360e7620bba42
Martin Thoma <info@martin-thoma.de>
2022-06-16 16:55:17 +0200

    DEV: Fix type annotations for add_bookmarks (#1000)

--- tests/test_merger.py
+++ tests/test_merger.py
@@ -38,7 +38,29 @@ def test_merge():
         merger.append(fh)
 
     bookmark = merger.add_bookmark("A bookmark", 0)
-    merger.add_bookmark("deeper", 0, parent=bookmark, italic=True, bold=True)
+    bm2 = merger.add_bookmark("deeper", 1, parent=bookmark, italic=True, bold=True)
+    merger.add_bookmark(
+        "Let's see", merger.pages[2], bm2, (255, 255, 0), True, True, "/FitBV", 12
+    )
+    merger.add_bookmark(
+        "The XYZ fit", 0, bookmark, (255, 0, 15), True, True, "/XYZ", 10, 20, 3
+    )
+    merger.add_bookmark(
+        "The FitH fit", 0, bookmark, (255, 0, 15), True, True, "/FitH", 10
+    )
+    merger.add_bookmark(
+        "The FitV fit", 0, bookmark, (255, 0, 15), True, True, "/FitV", 10
+    )
+    merger.add_bookmark(
+        "The FitR fit", 0, bookmark, (255, 0, 15), True, True, "/FitR", 10, 20, 30, 40
+    )
+    merger.add_bookmark("The FitB fit", 0, bookmark, (255, 0, 15), True, True, "/FitB")
+    merger.add_bookmark(
+        "The FitBH fit", 0, bookmark, (255, 0, 15), True, True, "/FitBH", 10
+    )
+    merger.add_bookmark(
+        "The FitBV fit", 0, bookmark, (255, 0, 15), True, True, "/FitBV", 10
+    )
     merger.add_metadata({"author": "Martin Thoma"})
     merger.add_named_destination("title", 0)
     merger.set_page_layout("/SinglePage")
@@ -65,6 +87,8 @@ def test_merge():
         "Baz",
         "foo",
     ]
+
+    # TODO: There seem to be no destionations for those links?
 
     # Clean up
     os.remove(tmp_path)
