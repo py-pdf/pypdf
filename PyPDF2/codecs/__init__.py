bd6976ab588f962b0f1f54ce6d09a65d0162204c
Martin Thoma <info@martin-thoma.de>
2022-06-18 10:26:21 +0200

    STY: Put pure data mappings in separate files

---
+++ PyPDF2/codecs/__init__.py
@@ -0,0 +1,11 @@
+from .pdfdoc import _pdfdoc_encoding
+from .std import _std_encoding
+from .symbol import _symbol_encoding
+from .zapfding import _zapfding_encoding
+
+__all__ = [
+    "_std_encoding",
+    "_symbol_encoding",
+    "_zapfding_encoding",
+    "_pdfdoc_encoding",
+]
