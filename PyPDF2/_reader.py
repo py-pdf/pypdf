9d26e7083959a530165be29b3bd6cdc38a2704bf
Martin Thoma <info@martin-thoma.de>
2022-06-19 22:12:34 +0200

    ENH: Add reader.pdf_version

--- PyPDF2/_reader.py
+++ PyPDF2/_reader.py
@@ -258,6 +258,13 @@ class PdfReader:
             raise PdfReadError("Wrong password")
 
     @property
+    def pdf_version(self) -> str:
+        loc = self.stream.tell()
+        pdf_file_version = self.stream.read(8).decode("utf-8")
+        self.stream.seek(loc, 0)  # return to where it was
+        return pdf_file_version
+
+    @property
     def metadata(self) -> Optional[DocumentInformation]:
         """
         Retrieve the PDF file's document information dictionary, if it exists.
