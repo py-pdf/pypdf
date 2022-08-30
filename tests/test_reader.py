b3d24c610a20a2a3f5b704cfc9e5902e0478e1b0
Ed Diamond <ediamondscience@gmail.com>
2022-08-30 10:51:51 +0000

    Added test for unreadable metadata and pdf file unreadablemetadata.pdf to test new condition

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -101,6 +101,17 @@ def test_read_metadata(pdf_path, expected):
 
 
 @pytest.mark.parametrize(
+    "pdf_path",
+    [RESOURCE_ROOT / "unreadablemetadata.pdf"]
+)
+def test_broken_meta_data(pdf_path):
+    with open(pdf_path, 'rb') as f:
+        reader = PdfReader(f)
+        with pytest.raises(PdfReadError):
+            reader.metadata
+
+
+@pytest.mark.parametrize(
     "src",
     [
         RESOURCE_ROOT / "crazyones.pdf",
