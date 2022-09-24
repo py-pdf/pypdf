9e16c11c6be2064627d3532cec9ea7578d112227
Martin Thoma <info@martin-thoma.de>
2022-09-24 17:05:10 +0200

    Switch argument order

--- PyPDF2/_merger.py
+++ PyPDF2/_merger.py
@@ -624,14 +624,14 @@ class PdfMerger:
     def add_outline_item(
         self,
         title: str,
-        pagenum: Optional[int] = None,  # deprecated
+        page_number: Optional[int] = None,  # deprecated
         parent: Union[None, TreeObject, IndirectObject] = None,
         color: Optional[Tuple[float, float, float]] = None,
         bold: bool = False,
         italic: bool = False,
         fit: FitType = "/Fit",
         *args: ZoomArgType,
-        page_number: Optional[int] = None,
+        pagenum: Optional[int] = None,
     ) -> IndirectObject:
         """
         Add an outline item (commonly referred to as a "Bookmark") to this PDF file.
