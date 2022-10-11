40b0f984bb8f3ffdd7296301dda5a20660ddc8a3
rogmann <github@rogmann.org>
2022-10-11 08:28:59 +0200

    MAINT: Executed black.

--- PyPDF2/_page.py
+++ PyPDF2/_page.py
@@ -1724,11 +1724,19 @@ class PageObject(DictionaryObject):
                     # To prevent sending letters instead of words we
                     # override the visitor temporarily.
                     visitor_text_before = visitor_text
-                    tm_matrix_before = [tm_matrix[0], tm_matrix[1],
-                        tm_matrix[2], tm_matrix[3], tm_matrix[4], tm_matrix[5]]
+                    tm_matrix_before = [
+                        tm_matrix[0],
+                        tm_matrix[1],
+                        tm_matrix[2],
+                        tm_matrix[3],
+                        tm_matrix[4],
+                        tm_matrix[5],
+                    ]
                     text_TJ = []
-                    def visitor_text(text, cm_matrix, tm_matrix, font_dict, font_size): 
+
+                    def visitor_text(text, cm_matrix, tm_matrix, font_dict, font_size):
                         text_TJ.append(text)
+
                 for op in operands[0]:
                     if isinstance(op, (str, bytes)):
                         process_operation(b"Tj", [op])
@@ -1741,7 +1749,13 @@ class PageObject(DictionaryObject):
                             process_operation(b"Tj", [" "])
                 if visitor_text is not None:
                     visitor_text = visitor_text_before
-                    visitor_text("".join(text_TJ), cm_matrix, tm_matrix_before, cmap[3], font_size)
+                    visitor_text(
+                        "".join(text_TJ),
+                        cm_matrix,
+                        tm_matrix_before,
+                        cmap[3],
+                        font_size,
+                    )
             elif operator == b"Do":
                 output += text
                 try:
