ea598dda99c6823d637e28f2f558ee5877499146
Martin Thoma <info@martin-thoma.de>
2023-01-04 11:55:02 +0100

    STY: Apply pre-commit

--- mutmut_config.py
+++ mutmut_config.py
@@ -7,5 +7,10 @@ See https://mutmut.readthedocs.io/en/latest/
 
 def pre_mutation(context):
     line = context.current_source_line.strip()
-    if "_codecs" in context.filename or "pragma: no cover" in line or "deprecate" in line or line.startswith("logger"):
+    if (
+        "_codecs" in context.filename
+        or "pragma: no cover" in line
+        or "deprecate" in line
+        or line.startswith("logger")
+    ):
         context.skip = True
