5c0b11d6f6dfe8f7744565c59cf0ffef9f706608
Martin Thoma <info@martin-thoma.de>
2023-01-07 19:02:59 +0100

    DOC: Use google style docstrings (#1534)
    The Google Docstring style is way more readable than ReST style docstrings. For this reason, this PR replaces ReST style docstrings by Google Style docstrings.
    
    Tools used:
    - pyment
    - pydocstyle
    
    Other changes:
    * DEV: Set python_use_unqualified_type_names=True
    * MAINT: Additionally, most instances of 'pragma: no-cover' were replaced by the more descriptive 'deprecated'.
    * DOC: Document how to document

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,10 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+Configuration file for the Sphinx documentation builder.
 
+This file only contains a selection of the most common options. For a full
+list see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -73,6 +74,8 @@ autodoc_default_options = {
     "undoc-members": True,
 }
 autodoc_inherit_docstrings = False
+autodoc_typehints_format = "short"
+python_use_unqualified_type_names = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
