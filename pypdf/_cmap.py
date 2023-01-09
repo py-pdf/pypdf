ea2aec580942945a936d6cbbc7595421de9bbcb9
pubpub-zz <4083478+pubpub-zz@users.noreply.github.com>
2023-01-09 19:24:39 +0100

    fix #1533
    error with multiple lines

--- pypdf/_cmap.py
+++ pypdf/_cmap.py
@@ -279,7 +279,7 @@ def parse_bfrange(
         fmt = b"%%0%dX" % (map_dict[-1] * 2)
         a = multiline_rg[0]  # a, b not in the current line
         b = multiline_rg[1]
-        for sq in lst[1:]:
+        for sq in lst[0:]:
             if sq == b"]":
                 closure_found = True
                 break
