5b1c0df6ec365db185af56f21747648a0726d190
Martin Thoma <info@martin-thoma.de>
2023-02-02 06:39:38 +0100

    Guard if no /Contents

--- pypdf/_writer.py
+++ pypdf/_writer.py
@@ -1830,6 +1830,8 @@ class PdfWriter:
         )
         for page in pages:
             page_ref = cast(DictionaryObject, self.get_object(page))
+            if "/Contents" not in page_ref:
+                return
             content = page_ref["/Contents"].get_object()
             if not isinstance(content, ContentStream):
                 content = ContentStream(content, page_ref)
