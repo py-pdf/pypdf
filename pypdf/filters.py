2205e9f310fe2bd9332f1e0ec01451d09d2344bd
Martin Thoma <info@martin-thoma.de>
2023-03-26 16:07:17 +0200

    DEV: ruff updates; activate --fix (#1747)

--- pypdf/filters.py
+++ pypdf/filters.py
@@ -63,7 +63,7 @@ if TYPE_CHECKING:
     except ImportError:
         # PEP 586 introduced typing.Literal with Python 3.8
         # For older Python versions, the backport typing_extensions is necessary:
-        from typing_extensions import Literal  # type: ignore[misc]
+        from typing_extensions import Literal  # type: ignore[misc, assignment]
 
 
 def decompress(data: bytes) -> bytes:
