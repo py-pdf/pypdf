a8a85e17799812d3e5e373196d208853d355e04f
Martin Thoma <info@martin-thoma.de>
2023-03-30 22:47:11 +0200

    Annotation module docstring

--- pypdf/annotations/__init__.py
+++ pypdf/annotations/__init__.py
@@ -1,4 +1,14 @@
-"""PDF annotations"""
+"""
+PDF specifies several annotation types which pypdf makes available here.
+
+The names of the annotations and their attributes do not reflect the names in
+the specification in all cases. For example, the PDF standard defines a
+'Square' annotation that does not actually need to be square. For this reason,
+pypdf calls it 'Rectangle'.
+
+At their core, all annotation types are DictionaryObjects. That means if pypdf
+does not implement a feature, users can easily extend the given functionality.
+"""
 
 
 from ._base import NO_FLAGS
