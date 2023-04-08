5b6b073a8ed3f4f1cd9d27e88793d85635513e55
Martin Thoma <info@martin-thoma.de>
2023-04-08 09:56:01 +0200

    Fast/slow split

--- tests/test_cmap.py
+++ tests/test_cmap.py
@@ -32,19 +32,6 @@ from . import get_pdf_from_url
             "tika-959173.pdf",
             True,
         ),
-        # L40, get_font_width_from_default
-        (
-            "https://corpora.tika.apache.org/base/docs/govdocs1/908/908104.pdf",
-            "tika-908104.pdf",
-            False,
-        ),
-        # bfchar_on_2_chars: issue #1293
-        (
-            "https://github.com/xyegithub/myBlog/raw/main/posts/c94b2364/paper_pdfs/ImageClassification/"
-            "2007%2CASurveyofImageClassificationBasedTechniques.pdf",
-            "ASurveyofImageClassificationBasedTechniques.pdf",
-            False,
-        ),
         # issue #1718:
         (
             "https://github.com/py-pdf/pypdf/files/10983477/Ballinasloe_WS.pdf",
@@ -53,7 +40,46 @@ from . import get_pdf_from_url
         ),
     ],
 )
-def test_text_extraction(caplog, url: str, name: str, strict: bool):
+def test_text_extraction_slow(caplog, url: str, name: str, strict: bool):
+    reader = PdfReader(BytesIO(get_pdf_from_url(url, name=name)), strict=strict)
+    for page in reader.pages:
+        page.extract_text()
+    assert caplog.text == ""
+
+
+@pytest.mark.enable_socket()
+@pytest.mark.parametrize(
+    ("url", "name", "strict"),
+    [
+        # bfchar_on_2_chars: issue #1293
+        (
+            "https://github.com/xyegithub/myBlog/raw/main/posts/c94b2364/paper_pdfs/ImageClassification/"
+            "2007%2CASurveyofImageClassificationBasedTechniques.pdf",
+            "ASurveyofImageClassificationBasedTechniques.pdf",
+            False,
+        ),
+        # L40, get_font_width_from_default
+        (
+            "https://corpora.tika.apache.org/base/docs/govdocs1/908/908104.pdf",
+            "tika-908104.pdf",
+            False,
+        ),
+        # multiline_bfrange / regression test for issue #1285:
+        (
+            "https://github.com/alexanderquispe/1REI05/raw/main/reports/report_1/"
+            "The%20lean%20times%20in%20the%20Peruvian%20economy.pdf",
+            "The%20lean%20times%20in%20the%20Peruvian%20economy.pdf",
+            False,
+        ),
+        (
+            "https://github.com/yxj-HGNwmb5kdp8ewr/yxj-HGNwmb5kdp8ewr.github.io/raw/master/files/"
+            "Giacalone%20Llobell%20Jaeger%20(2022)%20Food%20Qual%20Prefer.pdf",
+            "Giacalone.pdf",
+            False,
+        ),
+    ],
+)
+def test_text_extraction_fast(caplog, url: str, name: str, strict: bool):
     reader = PdfReader(BytesIO(get_pdf_from_url(url, name=name)), strict=strict)
     for page in reader.pages:
         page.extract_text()
@@ -72,27 +98,6 @@ def test_parse_encoding_advanced_encoding_not_implemented():
 
 
 @pytest.mark.enable_socket()
-def test_multiline_bfrange():
-    # non regression test for iss_1285
-    url = (
-        "https://github.com/alexanderquispe/1REI05/raw/main/reports/report_1/"
-        "The%20lean%20times%20in%20the%20Peruvian%20economy.pdf"
-    )
-    name = "tika-908104.pdf"
-    reader = PdfReader(BytesIO(get_pdf_from_url(url, name=name)))
-    for page in reader.pages:
-        page.extract_text()
-    url = (
-        "https://github.com/yxj-HGNwmb5kdp8ewr/yxj-HGNwmb5kdp8ewr.github.io/raw/master/files/"
-        "Giacalone%20Llobell%20Jaeger%20(2022)%20Food%20Qual%20Prefer.pdf"
-    )
-    name = "Giacalone.pdf"
-    reader = PdfReader(BytesIO(get_pdf_from_url(url, name=name)))
-    for page in reader.pages:
-        page.extract_text()
-
-
-@pytest.mark.enable_socket()
 def test_ascii_charset():
     # iss #1312
     url = "https://github.com/py-pdf/pypdf/files/9472500/main.pdf"
@@ -102,19 +107,21 @@ def test_ascii_charset():
 
 
 @pytest.mark.enable_socket()
-def test_iss1370():
-    url = "https://github.com/py-pdf/pypdf/files/9667138/cmap1370.pdf"
-    name = "cmap1370.pdf"
+@pytest.mark.parametrize(
+    ("url", "name", "page_nb"),
+    [
+        (
+            "https://github.com/py-pdf/pypdf/files/9667138/cmap1370.pdf",
+            "cmap1370.pdf",
+            0,
+        ),
+        ("https://github.com/py-pdf/pypdf/files/9712729/02voc.pdf", "02voc.pdf", 2),
+    ],
+    ids=["iss1370", "iss1379"],
+)
+def test_text_extraction_of_specific_pages(url: str, name: str, page_nb: int):
     reader = PdfReader(BytesIO(get_pdf_from_url(url, name=name)))
-    reader.pages[0].extract_text()
-
-
-@pytest.mark.enable_socket()
-def test_iss1379():
-    url = "https://github.com/py-pdf/pypdf/files/9712729/02voc.pdf"
-    name = "02voc.pdf"
-    reader = PdfReader(BytesIO(get_pdf_from_url(url, name=name)))
-    reader.pages[2].extract_text()
+    reader.pages[page_nb].extract_text()
 
 
 @pytest.mark.enable_socket()
