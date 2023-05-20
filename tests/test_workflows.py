8a04c8ccf8364fc51d77d52f00ca846d2a8c681b
pubpub-zz <4083478+pubpub-zz@users.noreply.github.com>
2023-05-20 16:46:03 +0200

    adjust threshold

--- tests/test_workflows.py
+++ tests/test_workflows.py
@@ -948,7 +948,7 @@ def test_replace_image(tmp_path):
     # very simple image distance evaluation
     diff = ImageChops.difference(reader2.pages[0].images[0].image, img)
     d = sum(diff.convert("L").getdata()) / (diff.size[0] * diff.size[1])
-    assert d < 1
+    assert d < 1.5
     writer.pages[0].images[0].replace(img, quality=20)
     diff = ImageChops.difference(writer.pages[0].images[0].image, img)
     d1 = sum(diff.convert("L").getdata()) / (diff.size[0] * diff.size[1])
