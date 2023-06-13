7921953d2036e9099bfa5ddc256116330da86b2f
Martin Thoma <info@martin-thoma.de>
2023-06-13 22:57:39 +0200

    Update pypdf/_utils.py

--- pypdf/_utils.py
+++ pypdf/_utils.py
@@ -514,19 +514,18 @@ class ImageFile(File):
         """
         Replace the Image with a new PIL image.
 
-    Args:
-        new_image (Image.Image): The new PIL image to replace the existing image.
-        **kwargs: Additional keyword arguments to pass to `Image.Image.save()`.
+        Args:
+            new_image (Image.Image): The new PIL image to replace the existing image.
+            **kwargs: Additional keyword arguments to pass to `Image.Image.save()`.
 
-    Raises:
-        TypeError: If the image is inline or in a PdfReader.
-        TypeError: If the image does not belong to a PdfWriter.
-        TypeError: If `new_image` is not a PIL Image.
+        Raises:
+            TypeError: If the image is inline or in a PdfReader.
+            TypeError: If the image does not belong to a PdfWriter.
+            TypeError: If `new_image` is not a PIL Image.
 
-    Note:
-        This method replaces the existing image with a new image. It is not allowed for inline images or images within a PdfReader.
-        The `kwargs` parameter allows passing additional parameters to `Image.Image.save()`, such as quality.
-    """
+        Note:
+            This method replaces the existing image with a new image. It is not allowed for inline images or images within a PdfReader.
+            The `kwargs` parameter allows passing additional parameters to `Image.Image.save()`, such as quality.
         """
         from PIL import Image
 
