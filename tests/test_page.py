5a42b96f67c67d2c5b8cbfbc01278f81edcd18bd
pubpub-zz <4083478+pubpub-zz@users.noreply.github.com>
2023-09-10 12:03:20 +0200

    BUG: Merge pages without resources (#2150)
    closes #2147

--- tests/test_page.py
+++ tests/test_page.py
@@ -1227,6 +1227,16 @@ def test_compression():
     )
 
 
+def test_merge_with_no_resources():
+    """Test for issue #2147"""
+    writer = PdfWriter()
+    p0 = writer.add_blank_page(900, 1200)
+    del p0["/Resources"]
+    p1 = writer.add_blank_page(900, 1200)
+    del p1["/Resources"]
+    writer.pages[0].merge_page(p1)
+
+
 def test_get_contents_from_nullobject():
     """Issue #2157"""
     writer = PdfWriter()
