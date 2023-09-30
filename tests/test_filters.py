f14fe77f614cfe47f6139265b26c5f03a3688766
Stefan <96178532+stefan6419846@users.noreply.github.com>
2023-09-30 15:28:50 +0200

    fix linting issues

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -285,7 +285,9 @@ for page in reader.pages:
         "It can be installed via 'pip install pypdf[image]'"
     ), exc.value.args[0]
 """)
-    result = subprocess.run([shutil.which("python"), source_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result = subprocess.run(  # noqa: S603, UP022
+        [shutil.which("python"), source_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
     assert result.returncode == 0
     assert result.stdout == b""
     assert result.stderr == b"Superfluous whitespace found in object header b'4' b'0'\n"
