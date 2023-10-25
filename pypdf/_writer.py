9abcfc2bdb128eddf7ab43bec00d701c4cf894ec
rdl <rdl@provexi.fr>
2023-10-25 17:56:35 +0200

    BUG: Error when filling a value with parentheses #2268

--- pypdf/_writer.py
+++ pypdf/_writer.py
@@ -920,7 +920,8 @@ class PdfWriter:
         else:  # /Tx
             txt = field.get("/V", "")
             sel = []
-
+        # Escape parentheses (Ref. 7.3.4.2 Literal Strings)
+        txt = txt.replace('(', r'\(').replace(')', r'\)')
         # Generate appearance stream
         ap_stream = f"q\n/Tx BMC \nq\n1 1 {rct.width - 1} {rct.height - 1} re\nW\nBT\n{da}\n".encode()
         for line_number, line in enumerate(txt.replace("\n", "\r").split("\r")):
