e9241ac795cea9b93f82741c57b1036de456d330
Stefan <96178532+stefan6419846@users.noreply.github.com>
2023-10-28 12:42:16 +0200

    BUG: Correctly handle image mode 1 with FlateDecode (#2249)
    Fixes #2248

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -22,6 +22,7 @@ from pypdf.filters import (
 from pypdf.generic import ArrayObject, DictionaryObject, NameObject, NumberObject
 
 from . import get_data_from_url
+from .test_encryption import HAS_AES
 from .test_images import image_similarity
 
 filter_inputs = (
@@ -637,3 +638,14 @@ def test_nested_device_n_color_space():
     name = "issue2240.pdf"
     reader = PdfReader(BytesIO(get_data_from_url(url, name=name)))
     reader.pages[0].images[0]
+
+
+@pytest.mark.enable_socket()
+@pytest.mark.skipif(not HAS_AES, reason="No AES implementation")
+def test_flate_decode_with_image_mode_1():
+    """From #2248"""
+    url = "https://github.com/py-pdf/pypdf/files/12847339/Prototype-Declaration-VDE4110-HYD-5000-20000-ZSS-DE.pdf"
+    name = "issue2248.pdf"
+    reader = PdfReader(BytesIO(get_data_from_url(url, name=name)))
+    for image in reader.pages[7].images:
+        _ = image
