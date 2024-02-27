178014e52c173518417211bad64d8fc3c9e6c999
Rak424 <fontaine.fr@gmail.com>
2024-02-27 19:05:05 +0100

    BUG: missing error on name without leading / (#2387)
    * handling name without leading /
    
    * init
    
    * type
    
    * optional
    
    * exception
    
    * test
    
    * test
    
    * test
    
    * missing /
    
    * wrong name value
    
    * wrong object
    
    * wrong object
    
    * Update tests/test_generic.py
    
    Co-authored-by: pubpub-zz <4083478+pubpub-zz@users.noreply.github.com>
    
    * indent
    
    * deprecate
    
    * ruff
    
    * deprecate_no_replacement
    
    * cleaning
    
    * fix formatting and version
    
    ---------
    
    Co-authored-by: francois <francois@rcdevs.com>
    Co-authored-by: pubpub-zz <4083478+pubpub-zz@users.noreply.github.com>
    Co-authored-by: Stefan <96178532+stefan6419846@users.noreply.github.com>

--- tests/test_generic.py
+++ tests/test_generic.py
@@ -219,9 +219,8 @@ def test_name_object(caplog):
 
     caplog.clear()
     b = BytesIO()
-    NameObject("hello").write_to_stream(b)
-    assert bytes(b.getbuffer()) == b"hello"
-    assert "Incorrect first char" in caplog.text
+    with pytest.raises(DeprecationWarning):
+        NameObject("hello").write_to_stream(b)
 
     caplog.clear()
     b = BytesIO()
