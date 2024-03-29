fd83294e24a4faf49c8e68a827ef51392e5d2c38
Stefan <96178532+stefan6419846@users.noreply.github.com>
2024-03-29 19:04:25 +0100

    attempt to pass env directly

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -1,4 +1,5 @@
 """Test the pypdf.filters module."""
+import os
 import shutil
 import string
 import subprocess
@@ -254,6 +255,9 @@ def test_issue_399():
 
 @pytest.mark.enable_socket()
 def test_image_without_pillow(tmp_path):
+    env = os.environ.copy()
+    env["COVERAGE_PROCESS_START"] = "pyproject.toml"
+
     name = "tika-914102.pdf"
     pdf_path = Path(__file__).parent / "pdf_cache" / name
     pdf_path_str = str(pdf_path.resolve()).replace("\\", "/")
@@ -281,7 +285,7 @@ for page in reader.pages:
     )
     result = subprocess.run(
         [shutil.which("python"), source_file],  # noqa: S603
-        capture_output=True,
+        capture_output=True, env=env,
     )
     assert result.returncode == 0
     assert result.stdout == b""
