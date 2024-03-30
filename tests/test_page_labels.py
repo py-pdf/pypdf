2512c74b814099696dec48cc4cb749f27a409481
Stefan <96178532+stefan6419846@users.noreply.github.com>
2024-03-30 20:00:46 +0100

    fix line length

--- tests/test_page_labels.py
+++ tests/test_page_labels.py
@@ -110,6 +110,8 @@ def test_index2label_kids():
     url = "https://www.bk.admin.ch/dam/bk/de/dokumente/terminologie/publikation_25_jahre_rtd.pdf.download.pdf/Terminologie_Epochen,%20Schwerpunkte,%20Umsetzungen.pdf"  # noqa: E501
     r = PdfReader(BytesIO(get_data_from_url(url=url, name="index2label_kids.pdf")))
     expected = [
-        "C1", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII",
+        "C1",
+        "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
+        "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII",
     ] + list(map(str, range(1, 284)))
     assert r.page_labels == expected
