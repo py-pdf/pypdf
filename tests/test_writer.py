3a2120cbe0877233257e9fffd1e3dabe61921844
pubpub-zz <4083478+pubpub-zz@users.noreply.github.com>
2024-04-02 16:52:09 +0200

    coverage

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -1504,14 +1504,15 @@ def test_update_form_fields(tmp_path):
         page_number=0,
         annotation=Link(target_page_index=1, rect=RectangleObject([0, 0, 100, 100])),
     )
+    writer.insert_blank_page(100, 100, 0)
     del writer.root_object["/AcroForm"]["/Fields"][1].get_object()["/DA"]
     del writer.root_object["/AcroForm"]["/Fields"][1].get_object()["/DR"]["/Font"]
     writer.update_page_form_field_values(
-        [writer.pages[0]],
+        [writer.pages[0], writer.pages[1]],
         {"Text1": "my Text1", "Text2": "ligne1\nligne2\nligne3"},
         auto_regenerate=False,
     )
-    assert b"/Helv " in writer.pages[0]["/Annots"][1]["/AP"]["/N"].get_data()
+    assert b"/Helv " in writer.pages[1]["/Annots"][1]["/AP"]["/N"].get_data()
     writer.update_page_form_field_values(
         None,
         {"Text1": "my Text1", "Text2": "ligne1\nligne2\nligne3"},
@@ -1519,13 +1520,6 @@ def test_update_form_fields(tmp_path):
     )
 
     Path(write_data_here).unlink()
-
-    writer = PdfWriter(clone_from=RESOURCE_ROOT / "crazyones.pdf")
-    writer.update_page_form_field_values(
-        [writer.pages[0]],
-        {"Text1": "my Text1", "Text2": "ligne1\nligne2\nligne3"},
-        auto_regenerate=False,
-    )
 
 
 @pytest.mark.enable_socket()
