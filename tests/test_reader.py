c3fe2e77168be417b80b09229686cb79ad129523
Marco Sirabella <marco@sirabella.org>
2024-05-18 10:18:22 -0400

    fixup! MAINT: Allow opening PdfReader as contextmanager

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -1289,15 +1289,15 @@ def test_reader(caplog):
     # iss #1273
     url = "https://github.com/py-pdf/pypdf/files/9464742/shiv_resume.pdf"
     name = "shiv_resume.pdf"
-    reader = PdfReader(BytesIO(get_data_from_url(url, name=name)))
-    assert "Previous trailer can not be read" in caplog.text
-    caplog.clear()
-    # first call requires some reparations...
-    reader.pages[0].extract_text()
-    caplog.clear()
-    # ...and now no more required
-    reader.pages[0].extract_text()
-    assert caplog.text == ""
+    with PdfReader(BytesIO(get_data_from_url(url, name=name))) as reader:
+        assert "Previous trailer can not be read" in caplog.text
+        caplog.clear()
+        # first call requires some reparations...
+        reader.pages[0].extract_text()
+        caplog.clear()
+        # ...and now no more required
+        reader.pages[0].extract_text()
+        assert caplog.text == ""
 
 
 @pytest.mark.enable_socket()
