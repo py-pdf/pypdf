d9841ddc40b1d1ab438ae474abec9d9351de1119
pubpub-zz <4083478+pubpub-zz@users.noreply.github.com>
2024-05-20 10:39:07 +0200

    Update pypdf/generic/_data_structures.py
    Co-authored-by: Stefan <96178532+stefan6419846@users.noreply.github.com>

--- pypdf/generic/_data_structures.py
+++ pypdf/generic/_data_structures.py
@@ -1162,7 +1162,6 @@ class ContentStream(DecodedStreamObject):
         assert tmp[:2] == b"ID"
         filtr = settings.get("/F", settings.get("/Filter", "not set"))
         savpos = stream.tell()
-        # import pdb;pdb.set_trace()
         # print("inline", stream.tell(),filtr,"*",settings)
         if isinstance(filtr, list):
             filtr = filtr[0]  # used forencoding
