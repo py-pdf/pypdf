160ab38090b8f4b96aafd0e15ca8a11ed3b77457
j-t-1 <120829237+j-t-1@users.noreply.github.com>
2024-06-25 08:57:28 +0100

    DOC: Tiny changes (#2723)
    Remove a superfluous PdfReader in an import clause.
    Small improvements to XmpInformation.

--- pypdf/xmp.py
+++ pypdf/xmp.py
@@ -1,7 +1,7 @@
 """
-Anything related to XMP metadata.
+Anything related to Extensible Metadata Platform (XMP) metadata.
 
-See https://en.wikipedia.org/wiki/Extensible_Metadata_Platform
+https://en.wikipedia.org/wiki/Extensible_Metadata_Platform
 """
 
 import datetime
@@ -202,8 +202,8 @@ def _getter_single(
 
 class XmpInformation(PdfObject):
     """
-    An object that represents Adobe XMP metadata.
-    Usually accessed by :py:attr:`xmp_metadata()<pypdf.PdfReader.xmp_metadata>`
+    An object that represents Extensible Metadata Platform (XMP) metadata.
+    Usually accessed by :py:attr:`xmp_metadata()<pypdf.PdfReader.xmp_metadata>`.
 
     Raises:
       PdfReadError: if XML is invalid
@@ -319,7 +319,7 @@ class XmpInformation(PdfObject):
     """An unformatted text string representing document keywords."""
 
     pdf_pdfversion = property(_getter_single(PDF_NAMESPACE, "PDFVersion"))
-    """The PDF file version, for example 1.0, 1.3."""
+    """The PDF file version, for example 1.0 or 1.3."""
 
     pdf_producer = property(_getter_single(PDF_NAMESPACE, "Producer"))
     """The name of the tool that created the PDF document."""
