ecc9f9902c28a67b880705b0fa93de86008468bb
j-t-1 <120829237+j-t-1@users.noreply.github.com>
2024-07-26 07:58:49 +0000

    STY: Reorder exported symbols
    Plus tiny change of a comment.

--- pypdf/annotations/_base.py
+++ pypdf/annotations/_base.py
@@ -12,8 +12,8 @@ class AnnotationDictionary(DictionaryObject, ABC):
         # "rect" should not be added here as PolyLine can automatically set it
         self[NameObject("/Type")] = NameObject("/Annot")
         # The flags was NOT added to the constructor on purpose: We expect that
-        #   most users don't want to change the default. If they want, they
-        #   can use the property. The default is 0.
+        # most users don't want to change the default. If they do, they
+        # can use the property. The default is 0.
 
     @property
     def flags(self) -> AnnotationFlag:
