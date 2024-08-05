0731c044b5df2aa18a7ea913388f649e02a4d07b
Oded Wurman <oded@acidremap.com>
2024-08-05 00:08:26 -0700

    Use test files from URL rather than resources

--- tests/test_text_extraction.py
+++ tests/test_text_extraction.py
@@ -159,12 +159,15 @@ def test_layout_mode_type0_font_widths():
     assert expected == reader.pages[0].extract_text(extraction_mode="layout")
 
 
+@pytest.mark.enable_socket()
 def test_layout_mode_indirect_sequence_font_widths():
     # Cover the situation where the sequence for font widths is an IndirectObject
     # ref https://github.com/py-pdf/pypdf/pull/2788
-    reader = PdfReader(RESOURCE_ROOT / "2788_example.pdf")
+    url = "https://github.com/user-attachments/files/16491621/2788_example.pdf"
+    reader = PdfReader(BytesIO(get_data_from_url(url, name="2788_example.pdf")))
     assert reader.pages[0].extract_text(extraction_mode="layout") == ""
-    reader = PdfReader(RESOURCE_ROOT / "2788_example_malformed.pdf")
+    url = "https://github.com/user-attachments/files/16491619/2788_example_malformed.pdf"
+    reader = PdfReader(BytesIO(get_data_from_url(url, name="2788_example_malformed.pdf")))
     with pytest.raises(ParseError) as exc:
         reader.pages[0].extract_text(extraction_mode="layout")
         assert str(exc.value).startswith("Invalid font width definition")
