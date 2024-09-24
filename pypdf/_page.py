2873b9eb9130a6529cc4e7003e37c3655bc3a7b6
Ryo Kamei <r-kamei@sixsquare.co.jp>
2024-09-24 18:55:45 +0900

    BUG: Missing spaces in extract_text() method (py-pdf#1328) calculation efficiency

--- pypdf/_page.py
+++ pypdf/_page.py
@@ -1985,12 +1985,13 @@ class PageObject(DictionaryObject):
                 process_operation(b"TL", [-operands[1]])
                 process_operation(b"Td", operands)
             elif operator == b"TJ":
+                # The space width may be smaller than the font width, so the width should be 95%.
+                _confirm_space_width = _space_width * 0.95
                 for op in operands[0]:
                     if isinstance(op, (str, bytes)):
                         process_operation(b"Tj", [op])
-                    # The space width may be smaller than the font width, so the width should be 95%.
                     if isinstance(op, (int, float, NumberObject, FloatObject)) and (
-                        (abs(float(op) / 0.95) >= _space_width)
+                        (abs(float(op)) >= _confirm_space_width)
                         and (len(text) > 0)
                         and (text[-1] != " ")
                     ):
