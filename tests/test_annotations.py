c77a0f5c4f1d643a10ad14fb29bd2caa81f2a62e
Ryo Kamei <r-kamei@sixsquare.co.jp>
2024-10-07 10:39:58 +0900

    Add tests

--- tests/test_annotations.py
+++ tests/test_annotations.py
@@ -93,6 +93,24 @@ def test_text_annotation(pdf_file_path):
 
 
 def test_free_text_annotation(pdf_file_path):
+    free_text_annotation = FreeText(
+        text="Hello World",
+        rect=(0, 0, 0, 0),
+    )
+    assert free_text_annotation["/DS"] == "font: normal normal 14pt Helvetica;text-align:left;color:#000000"
+    free_text_annotation = FreeText(
+        text="Hello World",
+        rect=(50, 550, 200, 650),
+        font="Arial",
+        bold=True,
+        italic=True,
+        font_size="20pt",
+        font_color="00ff00",
+        border_color=None,
+        background_color=None,
+    )
+    assert free_text_annotation["/DS"] == "font: italic bold 20pt Arial;text-align:left;color:#00ff00"
+
     # Arrange
     pdf_path = RESOURCE_ROOT / "crazyones.pdf"
     reader = PdfReader(pdf_path)
