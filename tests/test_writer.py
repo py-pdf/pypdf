72a883886ab83ff130098dd99e6f1110b2b7b264
Jiajie Han <zeohan.dxs@gmail.com>
2024-11-23 21:49:53 +0800

    ROB: Skip destination page being None in PdfWriter (#2963)
    Some PDFs happen `dest["/Page"] is None` which causes 
    
    > AttributeError: 'NoneType' object has no attribute 'indirect_reference'
    
    when merging files.

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -2484,6 +2484,16 @@ def test_append_pdf_with_dest_without_page(caplog):
     assert len(writer.named_destinations) == 3
 
 
+@pytest.mark.enable_socket
+def test_destination_page_is_none():
+    """Tests for #2963"""
+    url = "https://github.com/user-attachments/files/17879461/3.pdf"
+    name = "iss2963.pdf"
+    reader = PdfReader(BytesIO(get_data_from_url(url, name=name)))
+    writer = PdfWriter()
+    writer.append(reader)
+
+
 def test_stream_not_closed():
     """Tests for #2905"""
     src = RESOURCE_ROOT / "pdflatex-outline.pdf"
