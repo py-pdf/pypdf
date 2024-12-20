00e4df428ec3a1c0773591c8bc0e9132f3717a89
Stefan <96178532+stefan6419846@users.noreply.github.com>
2024-12-20 14:41:48 +0100

    fix indentation

--- pypdf/_page.py
+++ pypdf/_page.py
@@ -1984,8 +1984,8 @@ class PageObject(DictionaryObject):
             elif operator == b"Tw":
                 space_scale = 1.0 + float(operands[0] if operands else 0.0)
             elif operator == b"TL":
-               scale_x = math.sqrt(tm_matrix[0]**2 + tm_matrix[2]**2)
-               TL = float(operands[0] if operands else 0.0) * font_size * scale_x
+                scale_x = math.sqrt(tm_matrix[0]**2 + tm_matrix[2]**2)
+                TL = float(operands[0] if operands else 0.0) * font_size * scale_x
             elif operator == b"Tf":
                 if text != "":
                     output += text  # .translate(cmap)
