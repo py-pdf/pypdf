caa4d060f286fddc1c36d700b736973c615678db
j-t-1 <120829237+j-t-1@users.noreply.github.com>
2025-01-07 11:13:10 +0000

    STY: Use zip for conciseness
    Also tweak an assert statement.

--- pypdf/_utils.py
+++ pypdf/_utils.py
@@ -582,7 +582,9 @@ class Version:
         if not isinstance(other, Version):
             raise ValueError(f"Version cannot be compared against {type(other)}")
 
-        for (self_value, self_suffix), (other_value, other_suffix) in zip(self.components, other.components):
+        for item in zip(self.components, other.components):
+            (self_value, self_suffix) = item[0]
+            (other_value, other_suffix) = item[1]
 
             if self_value < other_value:
                 return True
