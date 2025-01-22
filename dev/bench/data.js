4c2874f3fcad1bed2ff1d883279a74da5c2b6568
github-action-benchmark <github@users.noreply.github.com>
2025-01-22 09:58:28 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for f1b471b8dcbf763b39cdaada85ad0946acc51f0f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1737538526116,
+  "lastUpdate": 1737539908693,
   "repoUrl": "https://github.com/py-pdf/pypdf",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -66335,6 +66335,72 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0025341173611422565",
             "extra": "mean: 739.3162984000071 msec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "120829237+j-t-1@users.noreply.github.com",
+            "name": "j-t-1",
+            "username": "j-t-1"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f1b471b8dcbf763b39cdaada85ad0946acc51f0f",
+          "message": "DEV: Remove TD004 from ignored Ruff rules (#3066)\n\nTD004 is a missing colon in TODO. Also remove an obsolete TODO.",
+          "timestamp": "2025-01-22T10:56:00+01:00",
+          "tree_id": "766ca1a3d30e2d1bdd371f7c53ee841366ddd8ef",
+          "url": "https://github.com/py-pdf/pypdf/commit/f1b471b8dcbf763b39cdaada85ad0946acc51f0f"
+        },
+        "date": 1737539906156,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/bench.py::test_page_operations",
+            "value": 3.4040409648062138,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00224091465218074",
+            "extra": "mean: 293.7684976000071 msec\nrounds: 5"
+          },
+          {
+            "name": "tests/bench.py::test_merge",
+            "value": 15.601181033977568,
+            "unit": "iter/sec",
+            "range": "stddev: 0.006439107407955315",
+            "extra": "mean: 64.0977114374941 msec\nrounds: 16"
+          },
+          {
+            "name": "tests/bench.py::test_text_extraction",
+            "value": 0.268901029136943,
+            "unit": "iter/sec",
+            "range": "stddev: 0.04372910547787652",
+            "extra": "mean: 3.7188403600000015 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/bench.py::test_read_string_from_stream_performance",
+            "value": 19.85268089833502,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0007169920156273594",
+            "extra": "mean: 50.37103075000147 msec\nrounds: 20"
+          },
+          {
+            "name": "tests/bench.py::test_image_new_property_performance",
+            "value": 0.08670488056636483,
+            "unit": "iter/sec",
+            "range": "stddev: 0.07243098905132224",
+            "extra": "mean: 11.5333761314 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/bench.py::test_large_compressed_image_performance",
+            "value": 1.3343235522766497,
+            "unit": "iter/sec",
+            "range": "stddev: 0.011543527194370582",
+            "extra": "mean: 749.443415199994 msec\nrounds: 5"
           }
         ]
       }
