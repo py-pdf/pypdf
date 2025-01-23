dbc6bfc5a0aa2958dff563212817b4bd9080017d
j-t-1 <120829237+j-t-1@users.noreply.github.com>
2025-01-23 10:42:42 +0000

    DEV: Remove ignoring Ruff rule PGH004

--- pypdf/constants.py
+++ pypdf/constants.py
@@ -42,7 +42,7 @@ class EncryptionDictAttributes:
     """
 
     R = "/R"  # number, required; revision of the standard security handler
-    O = "/O"  # 32-byte string, required  # noqa
+    O = "/O"  # 32-byte string, required  # noqa: E741
     U = "/U"  # 32-byte string, required
     P = "/P"  # integer flag, required; permitted operations
     ENCRYPT_METADATA = "/EncryptMetadata"  # boolean flag, optional
