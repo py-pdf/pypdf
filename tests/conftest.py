bd50ad5695a292d45caff27a079930255edc1548
j-t-1 <120829237+j-t-1@users.noreply.github.com>
2025-03-13 08:39:12 +0000

    STY: Remove unused pytest fixture
    Also remove unnecessary assignment before return.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,6 @@ from pathlib import Path
 
 import pytest
 
-from pypdf import PdfReader
-
 TESTS_ROOT = Path(__file__).parent.resolve()
 PROJECT_ROOT = TESTS_ROOT.parent
 RESOURCE_ROOT = PROJECT_ROOT / "resources"
@@ -20,11 +18,3 @@ def pdf_file_path(tmp_path_factory):
 @pytest.fixture(scope="session")
 def txt_file_path(tmp_path_factory):
     return tmp_path_factory.mktemp("pypdf-data") / f"{uuid.uuid4()}.txt"
-
-
-@pytest.fixture(scope="session")
-def pdf_reader_page():
-    """Gives a page that was retrieved from a PDF via PdfReader."""
-    pdf_path = RESOURCE_ROOT / "crazyones.pdf"
-    reader = PdfReader(pdf_path)
-    return reader.pages[0]
