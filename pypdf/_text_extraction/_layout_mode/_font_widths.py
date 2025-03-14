4432d32daa0dd9d242d9d4025bbb5cb8daaf3848
stefan6419846 <96178532+stefan6419846@users.noreply.github.com>
2025-03-14 08:50:32 +0100

    restore lost comments

--- pypdf/_text_extraction/_layout_mode/_font_widths.py
+++ pypdf/_text_extraction/_layout_mode/_font_widths.py
@@ -195,9 +195,11 @@ STANDARD_WIDTHS = {
         "~": 469,
     },
 }
+
+# 4 fonts, includes bold, oblique and bold oblique variants
 STANDARD_WIDTHS[
     "Courier"
-] = dict.fromkeys(STANDARD_WIDTHS["Times"], 600)
+] = dict.fromkeys(STANDARD_WIDTHS["Times"], 600)  # fixed width
 STANDARD_WIDTHS["ZapfDingbats"] = dict.fromkeys(STANDARD_WIDTHS["Times"], 1000)  # 1 font
 STANDARD_WIDTHS["Symbol"] = dict.fromkeys(STANDARD_WIDTHS["Times"], 500)  # 1 font
 # add aliases per table H.3 on page 1110 of the PDF 1.7 standard
