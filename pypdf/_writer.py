d4d9435085d17dc3031fb568ee12708a367b8cbd
j-t-1 <120829237+j-t-1@users.noreply.github.com>
2025-03-23 08:01:34 +0000

    DEV: Modify use of ignore Ruff rule ARG005
    ARG005: Unused lambda argument. If there is a single unused lambda argument, replacing it with an underscore does not trigger the rule.

--- pypdf/_writer.py
+++ pypdf/_writer.py
@@ -1838,7 +1838,7 @@ class PdfWriter(PdfDocCommon):
             self,
             page_destination.inc_parent_counter_outline
             if is_open
-            else (lambda _, __: 0),
+            else (lambda x, y: 0),
         )
         if "/Count" not in page_destination:
             page_destination[NameObject("/Count")] = NumberObject(0)
