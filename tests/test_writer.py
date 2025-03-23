58ef8d5a381dc951a5a4dcc54469030d78bc7030
Samuel Bradshaw <samuel.h.bradshaw@gmail.com>
2025-03-23 17:46:29 -0600

    Fix test

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -1388,13 +1388,13 @@ def test_new_removes():
 
     writer = PdfWriter()
     writer.clone_document_from_reader(reader)
-    writer.remove_text(font_names=['LiberationSans-Bold'])
+    writer.remove_text(font_names=["LiberationSans-Bold"])
     b = BytesIO()
     writer.write(b)
-    bb = bytes(b.getbuffer())
-    assert b"/Im0" in bb
-    assert b"Arbeitsschritt" not in bb
-    assert b"Modelltechnik" in bb
+    reader = PdfReader(b)
+    text = reader.pages[0].extract_text()
+    assert "Arbeitsschritt" not in text
+    assert "Modelltechnik" in text
 
     url = "https://github.com/py-pdf/pypdf/files/10832029/tt2.pdf"
     name = "GeoBaseWithComments.pdf"
