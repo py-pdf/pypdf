21dab5ceb0daffafa51b6da1c0ebcd0b2afa7683
Noah Jackowitz <noah.jackowitz@us.af.mil>
2025-03-28 11:39:09 -0600

    BUG: Garbled Text in Layout Extraction
    Pass ruff linting, add myself as a contributor
    
    Closes #3212

--- pypdf/_text_extraction/_layout_mode/_text_state_manager.py
+++ pypdf/_text_extraction/_layout_mode/_text_state_manager.py
@@ -177,7 +177,7 @@ class TextStateManager:
 
     def add_q(self) -> None:
         """Add another level to q_queue"""
-        self.font_stack.append((self.font, self.font_size),)
+        self.font_stack.append((self.font, self.font_size))
         self.q_depth.append(len(self.q_depth))
 
     def add_cm(self, *args: Any) -> TextStateManagerChainMapType:
