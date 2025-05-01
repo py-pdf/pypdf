7956534874e8698ffc65dddb7eb3699b3538275e
j-t-1 <120829237+j-t-1@users.noreply.github.com>
2025-05-01 18:21:33 +0100

    MAINT: Comform ASCIIHexDecode implementation to specification
    If the filter encounters the EOD marker after reading an odd number of hexadecimal digits, it shall behave as if a 0 (zero) followed the last digit [PDF 2.0, §7.4.2].

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -98,14 +98,14 @@ def test_flate_decode_decompress_with_array_params(params):
             string.ascii_letters.encode(),
         ),
         ("30313233343536373839>", string.digits.encode()),
-        # Check odd number of hexadecimal digits behaves as if a 0 (zero) followed the last digit
-        ("3938373635343332313>", string.digits[::-1].encode()),
         (
             "3  031323334353637   3839>",
             string.digits.encode(),
         ),  # Same as previous, but whitespaced
         ("30313233343536373839616263646566414243444546>", string.hexdigits.encode()),
         ("20090a0d0b0c>", string.whitespace.encode()),
+        # Odd number of hexadecimal digits behaves as if a 0 (zero) followed the last digit
+        ("3938373635343332313>", string.digits[::-1].encode()),
     ],
     ids=[
         "empty",
@@ -116,6 +116,7 @@ def test_flate_decode_decompress_with_array_params(params):
         "digits_whitespace",
         "hexdigits",
         "whitespace",
+        "odd_number",
     ],
 )
 def test_ascii_hex_decode_method(data, expected):
