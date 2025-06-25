880e95410e9f119228fc80ca63cdb1d563c6f840
Henning Körtel <henning.koertel@gmgcolor.com>
2025-06-25 11:11:16 +0200

    PI: FLATE decoding for streams with faulty tail bytes
    Some FLATE encoded streams of early Adobe Distiller / Pitstop versions are written with additionally added CR bytes to the PDF and calculate the faulty tail bytes into Length value of stream dict. Later then decoding fails. Solved with removing step by step tail bytes until decoding is successful.

--- pypdf/filters.py
+++ pypdf/filters.py
@@ -92,6 +92,23 @@ def decompress(data: bytes) -> bytes:
         return zlib.decompress(data)
     except zlib.error:
         try:
+            # First quick approach for known issue with faulty added bytes to the
+            # tail of the encoded stream from early Adobe Distiller or Pitstop versions
+            # with CR char as the default line separator (assumed by reverse engeneering)
+            # that breaks the decoding process in the end.
+            #
+            # Try first to cut off some of the tail byte by byte, however limited to not
+            # iterate through too many loops and kill the performance for large streams,
+            # to then allow the final fall back to run. Added this intermediate attempt,
+            # because starting from the head of the stream byte by byte kills completely
+            # the performace for large streams (e.g. 6 MB) with the tail-byte-issue
+            # and takes ages. This solution is really fast:
+            max_tail_cut_off_bytes: int = 8
+            for i in range(1, (max_tail_cut_off_bytes + 1 if len(data) > max_tail_cut_off_bytes else len(data))):
+                try:
+                    return zlib.decompressobj().decompress(data[:-i])
+                except zlib.error:
+                    pass
             # For larger files, use decompression object to enable buffered reading
             return zlib.decompressobj().decompress(data)
         except zlib.error:
