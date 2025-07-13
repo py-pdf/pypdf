dfc5f7910549600b2833b204bbd3ac6cad8eb3f8
Arya Nair <aryaajitnair@gmail.com>
2025-07-13 23:35:05 +0530

    REF: Remove type ignore comments from EmbeddedFile setter tests for cleaner code and improved readability

--- tests/test_embedded_files.py
+++ tests/test_embedded_files.py
@@ -20,21 +20,21 @@ def test_embedded_file_name_is_read_only():
     assert embedded_file.name == "test.txt"
 
     with pytest.raises(AttributeError):
-        embedded_file.name = "new_name.txt"  # type: ignore
+        embedded_file.name = "new_name.txt"
 
 
 def test_embedded_file_alternative_name_setter():
     writer = PdfWriter()
     embedded_file = writer.add_attachment("test.txt", b"content")
 
-    embedded_file.alternative_name = "Alternative Name"  # type: ignore
+    embedded_file.alternative_name = "Alternative Name"
     assert embedded_file.alternative_name == "Alternative Name"
 
-    embedded_file.alternative_name = None  # type: ignore
+    embedded_file.alternative_name = None
     assert embedded_file.pdf_object[NameObject("/UF")] == NullObject()
 
     pdf_string = create_string_object("PDF String")
-    embedded_file.alternative_name = pdf_string  # type: ignore
+    embedded_file.alternative_name = pdf_string
     assert embedded_file.alternative_name == "PDF String"
 
 
@@ -42,10 +42,10 @@ def test_embedded_file_description_setter():
     writer = PdfWriter()
     embedded_file = writer.add_attachment("test.txt", b"content")
 
-    embedded_file.description = "Test Description"  # type: ignore
+    embedded_file.description = "Test Description"
     assert embedded_file.description == "Test Description"
 
-    embedded_file.description = None  # type: ignore
+    embedded_file.description = None
     assert embedded_file.pdf_object[NameObject("/Desc")] == NullObject()
 
 
@@ -53,10 +53,10 @@ def test_embedded_file_subtype_setter():
     writer = PdfWriter()
     embedded_file = writer.add_attachment("test.txt", b"content")
 
-    embedded_file.subtype = "/text/plain"  # type: ignore
+    embedded_file.subtype = "/text/plain"
     assert embedded_file.subtype == "/text/plain"
 
-    embedded_file.subtype = None  # type: ignore
+    embedded_file.subtype = None
     assert embedded_file._embedded_file[NameObject("/Subtype")] == NullObject()
 
 
@@ -64,10 +64,10 @@ def test_embedded_file_size_setter():
     writer = PdfWriter()
     embedded_file = writer.add_attachment("test.txt", b"content")
 
-    embedded_file.size = 1024  # type: ignore
+    embedded_file.size = 1024
     assert embedded_file.size == 1024
 
-    embedded_file.size = None  # type: ignore
+    embedded_file.size = None
     assert embedded_file._ensure_params()[NameObject("/Size")] == NullObject()
 
 
@@ -76,10 +76,10 @@ def test_embedded_file_creation_date_setter():
     embedded_file = writer.add_attachment("test.txt", b"content")
 
     test_date = datetime(2023, 1, 1, 12, 0, 0)
-    embedded_file.creation_date = test_date  # type: ignore
+    embedded_file.creation_date = test_date
     assert embedded_file.creation_date == test_date
 
-    embedded_file.creation_date = None  # type: ignore
+    embedded_file.creation_date = None
     assert embedded_file._ensure_params()[NameObject("/CreationDate")] == NullObject()
 
 
@@ -88,10 +88,10 @@ def test_embedded_file_modification_date_setter():
     embedded_file = writer.add_attachment("test.txt", b"content")
 
     test_date = datetime(2023, 1, 2, 12, 0, 0)
-    embedded_file.modification_date = test_date  # type: ignore
+    embedded_file.modification_date = test_date
     assert embedded_file.modification_date == test_date
 
-    embedded_file.modification_date = None  # type: ignore
+    embedded_file.modification_date = None
     assert embedded_file._ensure_params()[NameObject("/ModDate")] == NullObject()
 
 
@@ -100,10 +100,10 @@ def test_embedded_file_checksum_setter():
     embedded_file = writer.add_attachment("test.txt", b"content")
 
     checksum_bytes = b"checksum_value"
-    embedded_file.checksum = checksum_bytes  # type: ignore
+    embedded_file.checksum = checksum_bytes
     assert embedded_file.checksum == checksum_bytes
 
-    embedded_file.checksum = None  # type: ignore
+    embedded_file.checksum = None
     assert embedded_file._ensure_params()[NameObject("/CheckSum")] == NullObject()
 
 
@@ -111,7 +111,7 @@ def test_embedded_file_associated_file_relationship_setter():
     writer = PdfWriter()
     embedded_file = writer.add_attachment("test.txt", b"content")
 
-    embedded_file.associated_file_relationship = "/Data"  # type: ignore
+    embedded_file.associated_file_relationship = "/Data"
     assert embedded_file.associated_file_relationship == "/Data"
 
 
@@ -120,14 +120,14 @@ def test_embedded_file_setters_integration():
     writer.add_blank_page(100, 100)
 
     embedded_file = writer.add_attachment("test.txt", b"Hello, World!")
-    embedded_file.alternative_name = "Alternative Name"  # type: ignore
-    embedded_file.description = "Test Description"  # type: ignore
-    embedded_file.subtype = "/text/plain"  # type: ignore
-    embedded_file.size = 13  # type: ignore
-    embedded_file.creation_date = datetime(2023, 1, 1, 12, 0, 0)  # type: ignore
-    embedded_file.modification_date = datetime(2023, 1, 2, 12, 0, 0)  # type: ignore
-    embedded_file.checksum = b"checksum123"  # type: ignore
-    embedded_file.associated_file_relationship = "/Data"  # type: ignore
+    embedded_file.alternative_name = "Alternative Name"
+    embedded_file.description = "Test Description"
+    embedded_file.subtype = "/text/plain"
+    embedded_file.size = 13
+    embedded_file.creation_date = datetime(2023, 1, 1, 12, 0, 0)
+    embedded_file.modification_date = datetime(2023, 1, 2, 12, 0, 0)
+    embedded_file.checksum = b"checksum123"
+    embedded_file.associated_file_relationship = "/Data"
 
     pdf_bytes = BytesIO()
     writer.write(pdf_bytes)
@@ -142,17 +142,17 @@ def test_embedded_file_null_object_handling():
     writer = PdfWriter()
     embedded_file = writer.add_attachment("test.txt", b"content")
 
-    embedded_file.alternative_name = "Name"  # type: ignore
-    embedded_file.description = "Description"  # type: ignore
-    embedded_file.subtype = "/text/plain"  # type: ignore
-    embedded_file.size = 1024  # type: ignore
-    embedded_file.checksum = b"checksum"  # type: ignore
+    embedded_file.alternative_name = "Name"
+    embedded_file.description = "Description"
+    embedded_file.subtype = "/text/plain"
+    embedded_file.size = 1024
+    embedded_file.checksum = b"checksum"
 
-    embedded_file.alternative_name = None  # type: ignore
-    embedded_file.description = None  # type: ignore
-    embedded_file.subtype = None  # type: ignore
-    embedded_file.size = None  # type: ignore
-    embedded_file.checksum = None  # type: ignore
+    embedded_file.alternative_name = None
+    embedded_file.description = None
+    embedded_file.subtype = None
+    embedded_file.size = None
+    embedded_file.checksum = None
 
     assert embedded_file.pdf_object[NameObject("/UF")] == NullObject()
     assert embedded_file.pdf_object[NameObject("/Desc")] == NullObject()
