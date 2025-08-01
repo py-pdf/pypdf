91afa958a331b9e8b0c0e3c2f9ff89a69e3a6ba0
j-t-1 <120829237+j-t-1@users.noreply.github.com>
2025-08-01 10:28:15 +0000

    Revert to individual lines

--- pypdf/filters.py
+++ pypdf/filters.py
@@ -630,21 +630,45 @@ class CCITTFaxDecode:
         tiff_header_struct = "<2shlh" + "hhll" * 8 + "h"
         tiff_header = struct.pack(
             tiff_header_struct,
-            b"II",                      # Byte order indication: Little endian
-            42,                         # Version number (always 42)
-            8,                          # Offset to the first image file directory (IFD)
-            8,                          # Number of tags in IFD
-            256, 4, 1, params.columns,  # ImageWidth, LONG, 1, width
-            257, 4, 1, params.rows,     # ImageLength, LONG, 1, length
-            258, 3, 1, 1,               # BitsPerSample, SHORT, 1, 1
-            259, 3, 1, params.group,    # Compression, SHORT, 1, Compression Type
-            262, 3, 1, 0,               # Thresholding, SHORT, 1, 0 = BlackIs1
-            273, 4, 1, struct.calcsize(
+            b"II",  # Byte order indication: Little endian
+            42,     # Version number (always 42)
+            8,      # Offset to the first image file directory (IFD)
+            8,      # Number of tags in IFD
+            256,    # ImageWidth, LONG, 1, width
+            4,
+            1,
+            params.columns,  
+            257,    # ImageLength, LONG, 1, length
+            4,
+            1,
+            params.rows,     
+            258,    # BitsPerSample, SHORT, 1, 1
+            3,
+            1,
+            1,              
+            259,    # Compression, SHORT, 1, compression Type
+            3,
+            1,
+            params.group,    
+            262,    # Thresholding, SHORT, 1, 0 = BlackIs1
+            3,
+            1,
+            0,               
+            273,    # StripOffsets, LONG, 1, length of header
+            4,
+            1,
+              struct.calcsize(
                 tiff_header_struct
-            ),                          # StripOffsets, LONG, 1, length of header
-            278, 4, 1, params.rows,     # RowsPerStrip, LONG, 1, length
-            279, 4, 1, img_size,        # StripByteCounts, LONG, 1, size of image
-            0,  # last IFD
+            ),                         
+            278,    # RowsPerStrip, LONG, 1, length
+            4,
+            1,
+            params.rows,    
+            279,    # StripByteCounts, LONG, 1, size of image
+            4,
+            1,
+            img_size,      
+            0,      # last IFD
         )
 
         return tiff_header + data
