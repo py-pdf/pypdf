8251794c9619963fa12a9984fd725e5f5fd0c2cc
j-t-1 <120829237+j-t-1@users.noreply.github.com>
2025-08-05 11:24:38 +0000

    Update comment

--- pypdf/filters.py
+++ pypdf/filters.py
@@ -653,7 +653,7 @@ class CCITTFaxDecode:
             262,    # Thresholding, SHORT, 1, 0 = BlackIs1
             3,
             1,
-            0 if params.BlackIs1 is True or params.BlackIs1 is None else 1,  # Thresholding, SHORT, 1, 0 = WhiteIs0
+            0 if params.BlackIs1 is True or params.BlackIs1 is None else 1,  # Thresholding, SHORT, 1, 0 = BlackIs1
             273,    # StripOffsets, LONG, 1, length of header
             4,
             1,
