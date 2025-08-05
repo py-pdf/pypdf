decb94524718f519db85cde8e316efb57d858d3e
j-t-1 <120829237+j-t-1@users.noreply.github.com>
2025-08-05 20:01:47 +0100

    Fix error

--- pypdf/filters.py
+++ pypdf/filters.py
@@ -605,14 +605,14 @@ class CCITTFaxDecode:
                     if CCITT.K in decode_parm:
                         k = decode_parm[CCITT.K].get_object()
                     if CCITT.BlackIs1 in decode_parm:
-                        black_is_1 = decode_parm[CCITT.BlackIs1].get_object()
+                        black_is_1 = decode_parm[CCITT.BlackIs1].get_object().value()
             else:
                 if CCITT.COLUMNS in parameters_unwrapped:
                     columns = parameters_unwrapped[CCITT.COLUMNS].get_object()  # type: ignore
                 if CCITT.K in parameters_unwrapped:
                     k = parameters_unwrapped[CCITT.K].get_object()  # type: ignore
                 if CCITT.BlackIs1 in parameters_unwrapped:
-                    black_is_1 = parameters_unwrapped[CCITT.BlackIs1].get_object()  # type: ignore
+                    black_is_1 = parameters_unwrapped[CCITT.BlackIs1].get_object().value()  # type: ignore
 
         return CCITTParameters(K=k, columns=columns, rows=int(rows), BlackIs1=black_is_1)
 
