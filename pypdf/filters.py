23791685498018dd7535827b5732b203d4de71ee
j-t-1 <120829237+j-t-1@users.noreply.github.com>
2025-08-06 13:30:08 +0100

    Update pypdf/filters.py
    Co-authored-by: Stefan <96178532+stefan6419846@users.noreply.github.com>

--- pypdf/filters.py
+++ pypdf/filters.py
@@ -603,7 +603,7 @@ class CCITTFaxDecode:
             if isinstance(parameters_unwrapped, ArrayObject):
                 for decode_parm in parameters_unwrapped:
                     if CCITT.K in decode_parm:
-                        k = decode_parm[CCITT.K].get_object()
+                        ccitt_parameters.k = decode_parm[CCITT.K].get_object()
                     if CCITT.COLUMNS in decode_parm:
                         columns = decode_parm[CCITT.COLUMNS].get_object()
                     if CCITT.BLACK_IS_1 in decode_parm:
