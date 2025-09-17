962c9765663d34dba75e13e06e73787d9823600f
PJBrs <pjbrs@floorenpj.nl>
2025-09-17 17:45:18 +0200

    MAINT: _appearance_stream: Make variables more clear and readable
    Code in _appearance_stream used various rather cryptic variable names
    that, for some coders, made it hard to understand what the code was
    doing. This patch tries to clarify those variable names to make it
    easier to understand what's going on, and make it easier later on
    to add functionality. Overview of the changes:
    txt             --> text
    sel             --> selection
    da              --> default_appearance
    font_full_rev   --> font_glyph_byte_map
    rct             --> rectangle
    k, v            --> key, value
    enc_line        --> encoded_line
    af              --> acro_form
    dr              --> document_resources / document_font_resources
    font_res        --> font_resource
    
    Furthermore, I undid some abbreviated imports:
     - AnnotationDictionaryAttributes no longer as AA
     - FieldDictionaryAttributes no longer as FA

--- pypdf/generic/_appearance_stream.py
+++ pypdf/generic/_appearance_stream.py
@@ -2,8 +2,7 @@ from typing import Any, Optional, Union, cast
 
 from .._cmap import _default_fonts_space_width, build_char_map_from_dict
 from .._utils import logger_warning
-from ..constants import AnnotationDictionaryAttributes as AA
-from ..constants import FieldDictionaryAttributes as FA
+from ..constants import AnnotationDictionaryAttributes, FieldDictionaryAttributes
 from ..generic import (
     DecodedStreamObject,
     DictionaryObject,
@@ -24,50 +23,53 @@ class TextStreamAppearance(DecodedStreamObject):
 
     def __init__(
         self,
-        txt: str = "",
-        sel: Optional[list[str]] = None,
-        da: str = "",
-        font_full_rev: Optional[dict[str, bytes]] = None,
-        rct: Union[RectangleObject, tuple[float, float, float, float]] = (0.0, 0.0, 0.0, 0.0),
+        text: str = "",
+        selection: Optional[list[str]] = None,
+        default_appearance: str = "",
+        font_glyph_byte_map: Optional[dict[str, bytes]] = None,
+        rectangle: Union[RectangleObject, tuple[float, float, float, float]] = (0.0, 0.0, 0.0, 0.0),
         font_height: float = 0,
         y_offset: float = 0,
     ) -> None:
         super().__init__()
-        font_full_rev = font_full_rev or {}
-        if isinstance(rct, tuple):
-            rct = RectangleObject(rct)
-        ap_stream = f"q\n/Tx BMC \nq\n1 1 {rct.width - 1} {rct.height - 1} re\nW\nBT\n{da}\n".encode()
-        for line_number, line in enumerate(txt.replace("\n", "\r").split("\r")):
-            if sel and line in sel:
+        font_glyph_byte_map = font_glyph_byte_map or {}
+        if isinstance(rectangle, tuple):
+            rectangle = RectangleObject(rectangle)
+        ap_stream = (
+            f"q\n/Tx BMC \nq\n1 1 {rectangle.width - 1} {rectangle.height - 1} "
+            f"re\nW\nBT\n{default_appearance}\n"
+        ).encode()
+        for line_number, line in enumerate(text.replace("\n", "\r").split("\r")):
+            if selection and line in selection:
                 # may be improved but cannot find how to get fill working => replaced with lined box
                 ap_stream += (
-                    f"1 {y_offset - (line_number * font_height * 1.4) - 1} {rct.width - 2} {font_height + 2} re\n"
-                    f"0.5 0.5 0.5 rg s\n{da}\n"
+                    f"1 {y_offset - (line_number * font_height * 1.4) - 1} {rectangle.width - 2} {font_height + 2} re\n"
+                    f"0.5 0.5 0.5 rg s\n{default_appearance}\n"
                 ).encode()
             if line_number == 0:
                 ap_stream += f"2 {y_offset} Td\n".encode()
             else:
                 # Td is a relative translation
                 ap_stream += f"0 {-font_height * 1.4} Td\n".encode()
-            enc_line: list[bytes] = [
-                font_full_rev.get(c, c.encode("utf-16-be")) for c in line
+            encoded_line: list[bytes] = [
+                font_glyph_byte_map.get(c, c.encode("utf-16-be")) for c in line
             ]
-            if any(len(c) >= 2 for c in enc_line):
-                ap_stream += b"<" + (b"".join(enc_line)).hex().encode() + b"> Tj\n"
+            if any(len(c) >= 2 for c in encoded_line):
+                ap_stream += b"<" + (b"".join(encoded_line)).hex().encode() + b"> Tj\n"
             else:
-                ap_stream += b"(" + b"".join(enc_line) + b") Tj\n"
+                ap_stream += b"(" + b"".join(encoded_line) + b") Tj\n"
         ap_stream += b"ET\nQ\nEMC\nQ\n"
 
         self[NameObject("/Type")] = NameObject("/XObject")
         self[NameObject("/Subtype")] = NameObject("/Form")
-        self[NameObject("/BBox")] = rct
+        self[NameObject("/BBox")] = rectangle
         self.set_data(ByteStringObject(ap_stream))
         self[NameObject("/Length")] = NumberObject(len(ap_stream))
 
     @classmethod
     def from_text_annotation(
         cls,
-        af: DictionaryObject,  # _root_object[CatalogDictionary.ACRO_FORM])
+        acro_form: DictionaryObject,  # _root_object[CatalogDictionary.ACRO_FORM])
         field: DictionaryObject,
         annotation: DictionaryObject,
         font_name: str = "",
@@ -76,19 +78,19 @@ class TextStreamAppearance(DecodedStreamObject):
         """Creates a TextStreamAppearance object from a given text field annotation."""
 
         # Calculate rectangle dimensions
-        _rct = cast(RectangleObject, annotation[AA.Rect])
-        rct = RectangleObject((0, 0, abs(_rct[2] - _rct[0]), abs(_rct[3] - _rct[1])))
+        _rectangle = cast(RectangleObject, annotation[AnnotationDictionaryAttributes.Rect])
+        rectangle = RectangleObject((0, 0, abs(_rectangle[2] - _rectangle[0]), abs(_rectangle[3] - _rectangle[1])))
 
         # Extract font information
-        da = annotation.get_inherited(
-            AA.DA,
-            af.get(AA.DA, None),
+        default_appearance = annotation.get_inherited(
+            AnnotationDictionaryAttributes.DA,
+            acro_form.get(AnnotationDictionaryAttributes.DA, None),
         )
-        if da is None:
-            da = TextStringObject("/Helv 0 Tf 0 g")
+        if default_appearance is None:
+            default_appearance = TextStringObject("/Helv 0 Tf 0 g")
         else:
-            da = da.get_object()
-        font_properties = da.replace("\n", " ").replace("\r", " ").split(" ")
+            default_appearance = default_appearance.get_object()
+        font_properties = default_appearance.replace("\n", " ").replace("\r", " ").split(" ")
         font_properties = [x for x in font_properties if x != ""]
         if font_name:
             font_properties[font_properties.index("Tf") - 2] = font_name
@@ -100,89 +102,94 @@ class TextStreamAppearance(DecodedStreamObject):
             else float(font_properties[font_properties.index("Tf") - 1])
         )
         if font_height == 0:
-            if field.get(FA.Ff, 0) & FA.FfBits.Multiline:
+            if field.get(FieldDictionaryAttributes.Ff, 0) & FieldDictionaryAttributes.FfBits.Multiline:
                 font_height = DEFAULT_FONT_HEIGHT_IN_MULTILINE
             else:
-                font_height = rct.height - 2
+                font_height = rectangle.height - 2
         font_properties[font_properties.index("Tf") - 1] = str(font_height)
-        da = " ".join(font_properties)
-        y_offset = rct.height - 1 - font_height
+        default_appearance = " ".join(font_properties)
+        y_offset = rectangle.height - 1 - font_height
 
         # Retrieve font information from local DR ...
-        dr: Any = cast(
+        document_resources: Any = cast(
             DictionaryObject,
             cast(
                 DictionaryObject,
                 annotation.get_inherited(
                     "/DR",
-                    af.get("/DR", DictionaryObject()),
+                    acro_form.get("/DR", DictionaryObject()),
                 ),
             ).get_object(),
         )
-        dr = dr.get("/Font", DictionaryObject()).get_object()
+        document_font_resources = document_resources.get("/Font", DictionaryObject()).get_object()
         # _default_fonts_space_width keys is the list of Standard fonts
-        if font_name not in dr and font_name not in _default_fonts_space_width:
+        if font_name not in document_font_resources and font_name not in _default_fonts_space_width:
             # ...or AcroForm dictionary
-            dr = cast(
+            document_resources = cast(
                 dict[Any, Any],
-                af.get("/DR", {}),
+                acro_form.get("/DR", {}),
             )
-            dr = dr.get_object().get("/Font", DictionaryObject()).get_object()
-        font_res = dr.get(font_name, None)
-        if not is_null_or_none(font_res):
-            font_res = cast(DictionaryObject, font_res.get_object())
+            document_font_resources = document_resources.get_object().get("/Font", DictionaryObject()).get_object()
+        font_resource = document_font_resources.get(font_name, None)
+        if not is_null_or_none(font_resource):
+            font_resource = cast(DictionaryObject, font_resource.get_object())
             _font_subtype, _, font_encoding, font_map = build_char_map_from_dict(
-                200, font_res
+                200, font_resource
             )
             try:  # remove width stored in -1 key
                 del font_map[-1]
             except KeyError:
                 pass
-            font_full_rev: dict[str, bytes]
+            font_glyph_byte_map: dict[str, bytes]
             if isinstance(font_encoding, str):
-                font_full_rev = {
-                    v: k.encode(font_encoding) for k, v in font_map.items()
+                font_glyph_byte_map = {
+                    value: key.encode(font_encoding) for key, value in font_map.items()
                 }
             else:
-                font_full_rev = {v: bytes((k,)) for k, v in font_encoding.items()}
-                font_encoding_rev = {v: bytes((k,)) for k, v in font_encoding.items()}
+                font_glyph_byte_map = {value: bytes((key,)) for key, value in font_encoding.items()}
+                font_encoding_rev = {value: bytes((key,)) for key, value in font_encoding.items()}
                 for key, value in font_map.items():
-                    font_full_rev[value] = font_encoding_rev.get(key, key)
+                    font_glyph_byte_map[value] = font_encoding_rev.get(key, key)
         else:
             logger_warning(f"Font dictionary for {font_name} not found.", __name__)
-            font_full_rev = {}
+            font_glyph_byte_map = {}
 
         # Retrieve field text and selected values
-        field_flags = field.get(FA.Ff, 0)
-        if field.get(FA.FT, "/Tx") == "/Ch" and field_flags & FA.FfBits.Combo == 0:
-            txt = "\n".join(annotation.get_inherited(FA.Opt, []))
-            sel = field.get("/V", [])
-            if not isinstance(sel, list):
-                sel = [sel]
+        field_flags = field.get(FieldDictionaryAttributes.Ff, 0)
+        if (
+                field.get(FieldDictionaryAttributes.FT, "/Tx") == "/Ch" and
+                field_flags & FieldDictionaryAttributes.FfBits.Combo == 0
+        ):
+            text = "\n".join(annotation.get_inherited(FieldDictionaryAttributes.Opt, []))
+            selection = field.get("/V", [])
+            if not isinstance(selection, list):
+                selection = [selection]
         else:  # /Tx
-            txt = field.get("/V", "")
-            sel = []
+            text = field.get("/V", "")
+            selection = []
         # Escape parentheses (PDF 1.7 reference, table 3.2, Literal Strings)
-        txt = txt.replace("\\", "\\\\").replace("(", r"\(").replace(")", r"\)")
+        text = text.replace("\\", "\\\\").replace("(", r"\(").replace(")", r"\)")
 
         # Create the TextStreamAppearance instance
         new_appearance_stream = cls(
-            txt, sel, da, font_full_rev, rct, font_height, y_offset
+            text, selection, default_appearance, font_glyph_byte_map, rectangle, font_height, y_offset
         )
 
-        if AA.AP in annotation:
-            for k, v in cast(DictionaryObject, annotation[AA.AP]).get("/N", {}).items():
-                if k not in {"/BBox", "/Length", "/Subtype", "/Type", "/Filter"}:
-                    new_appearance_stream[k] = v
+        if AnnotationDictionaryAttributes.AP in annotation:
+            for key, value in (
+                cast(DictionaryObject, annotation[AnnotationDictionaryAttributes.AP]).get("/N", {}).items()
+            ):
+                if key not in {"/BBox", "/Length", "/Subtype", "/Type", "/Filter"}:
+                    new_appearance_stream[key] = value
 
         # Update Resources with font information if necessary
-        if font_res is not None:
+        if font_resource is not None:
             new_appearance_stream[NameObject("/Resources")] = DictionaryObject(
                 {
                     NameObject("/Font"): DictionaryObject(
                         {
                             NameObject(font_name): getattr(
-                                font_res, "indirect_reference", font_res
+                                font_resource, "indirect_reference", font_resource
                             )
                         }
                     )
