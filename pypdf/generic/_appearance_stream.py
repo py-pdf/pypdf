82d02ced3ff6809ed1d7786517db2af5a02b8fb8
PJBrs <pjbrs@floorenpj.nl>
2025-09-18 17:56:04 +0200

    MAINT: _appearance_stream: Factor out generation of text appearance
    This patch makes the code for producing the appearance stream data
    into a separate method.

--- pypdf/generic/_appearance_stream.py
+++ pypdf/generic/_appearance_stream.py
@@ -20,6 +20,41 @@ class TextStreamAppearance(DecodedStreamObject):
     A class representing the appearance stream for a text-based form field.
     This class is similar in form to the FreeText class in pypdf.
     """
+    def _appearance_stream_data(
+        self,
+        text: str = "",
+        selection: Optional[list[str]] = None,
+        default_appearance: str = "",
+        font_glyph_byte_map: Optional[dict[str, bytes]] = None,
+        rect: Union[RectangleObject, tuple[float, float, float, float]] = (0.0, 0.0, 0.0, 0.0),
+        font_size: float = 0,
+        y_offset: float = 0,
+    ) -> bytes:
+        font_glyph_byte_map = font_glyph_byte_map or {}
+        if isinstance(rect, tuple):
+            rect = RectangleObject(rect)
+        ap_stream = f"q\n/Tx BMC \nq\n1 1 {rect.width - 1} {rect.height - 1} re\nW\nBT\n{default_appearance}\n".encode()
+        for line_number, line in enumerate(text.replace("\n", "\r").split("\r")):
+            if selection and line in selection:
+                # may be improved but cannot find how to get fill working => replaced with lined box
+                ap_stream += (
+                    f"1 {y_offset - (line_number * font_size * 1.4) - 1} {rect.width - 2} {font_size + 2} re\n"
+                    f"0.5 0.5 0.5 rg s\n{default_appearance}\n"
+                ).encode()
+            if line_number == 0:
+                ap_stream += f"2 {y_offset} Td\n".encode()
+            else:
+                # Td is a relative translation
+                ap_stream += f"0 {-font_size * 1.4} Td\n".encode()
+            encoded_line: list[bytes] = [
+                font_glyph_byte_map.get(c, c.encode("utf-16-be")) for c in line
+            ]
+            if any(len(c) >= 2 for c in encoded_line):
+                ap_stream += b"<" + (b"".join(encoded_line)).hex().encode() + b"> Tj\n"
+            else:
+                ap_stream += b"(" + b"".join(encoded_line) + b") Tj\n"
+        ap_stream += b"ET\nQ\nEMC\nQ\n"
+        return ap_stream
 
     def __init__(
         self,
@@ -34,34 +69,15 @@ class TextStreamAppearance(DecodedStreamObject):
         font_glyph_byte_map = font_glyph_byte_map or {}
         if isinstance(rect, tuple):
             rect = RectangleObject(rect)
-        ap_stream = f"q\n/Tx BMC \nq\n1 1 {rect.width - 1} {rect.height - 1} re\nW\nBT\n{default_appearance}\n".encode()
-        for line_number, line in enumerate(text.replace("\n", "\r").split("\r")):
-            if selection and line in selection:
-                # may be improved but cannot find how to get fill working => replaced with lined box
-                ap_stream += (
-                    f"1 {y_offset - (line_number * font_height * 1.4) - 1} {rect.width - 2} {font_height + 2} re\n"
-                    f"0.5 0.5 0.5 rg s\n{default_appearance}\n"
-                ).encode()
-            if line_number == 0:
-                ap_stream += f"2 {y_offset} Td\n".encode()
-            else:
-                # Td is a relative translation
-                ap_stream += f"0 {-font_height * 1.4} Td\n".encode()
-            encoded_line: list[bytes] = [
-                font_glyph_byte_map.get(c, c.encode("utf-16-be")) for c in line
-            ]
-            if any(len(c) >= 2 for c in encoded_line):
-                ap_stream += b"<" + (b"".join(encoded_line)).hex().encode() + b"> Tj\n"
-            else:
-                ap_stream += b"(" + b"".join(encoded_line) + b") Tj\n"
-        ap_stream += b"ET\nQ\nEMC\nQ\n"
-
+        ap_stream_data = self._appearance_stream_data(
+            text, selection, default_appearance, font_glyph_byte_map, rect, font_height, y_offset,
+        )
         super().__init__()
         self[NameObject("/Type")] = NameObject("/XObject")
         self[NameObject("/Subtype")] = NameObject("/Form")
         self[NameObject("/BBox")] = rect
-        self.set_data(ByteStringObject(ap_stream))
-        self[NameObject("/Length")] = NumberObject(len(ap_stream))
+        self.set_data(ByteStringObject(ap_stream_data))
+        self[NameObject("/Length")] = NumberObject(len(ap_stream_data))
 
     @classmethod
     def from_text_annotation(
