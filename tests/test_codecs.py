93f4ab39a8f9b5d60b1e334fba73231c0a488427
stefan6419846 <96178532+stefan6419846@users.noreply.github.com>
2025-10-22 17:43:39 +0200

    add test file URL

--- tests/test_codecs.py
+++ tests/test_codecs.py
@@ -86,7 +86,7 @@ def test_lzw_decoder_large_stream_performance(caplog):
 
 @pytest.mark.enable_socket
 def test_lzw_decoder__output_limit():
-    url = ""
+    url = "https://github.com/user-attachments/files/23057035/lzw__output_limit.pdf"
     name = "lzw__output_limit.pdf"
     reader = PdfReader(BytesIO(get_data_from_url(url, name=name)))
     page = reader.pages[0]
