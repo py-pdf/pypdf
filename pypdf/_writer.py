97c2524594660763aa49ef9f75523723704a5042
j-t-1 <120829237+j-t-1@users.noreply.github.com>
2025-10-29 12:05:34 +0000

    Add return None

--- pypdf/_writer.py
+++ pypdf/_writer.py
@@ -3206,6 +3206,7 @@ class PdfWriter(PdfDocCommon):
                 o = cast(TreeObject, o["/Next"])
             else:
                 return None
+        return None
 
     def reset_translation(
         self, reader: Union[None, PdfReader, IndirectObject] = None
