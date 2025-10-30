996366c2594d07ca15970906b226f9b05b5adef6
Ievgen Kapinos <ievgen.kapinos@gmail.com>
2025-10-30 22:19:37 +0100

    Polishing

--- pypdf/_page.py
+++ pypdf/_page.py
@@ -211,8 +211,8 @@ class Transformation:
         Example:
             >>> from pypdf import Transformation
             >>> from pypdf._page import PageObject
-            >>> height = 40
-            >>> width = 50
+            >>> height, width = 40, 50
+            >>> page = PageObject()
             >>> op = Transformation((1, 0, 0, -1, 0, height)) # vertical mirror
             >>> op = Transformation().transform(Transformation((-1, 0, 0, 1, width, 0)))  # horizontal mirror
             >>> page.add_transformation(op)
