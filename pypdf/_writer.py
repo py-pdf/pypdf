66c291a8d676cf1d78180905c1be87d5dc04ac9b
j-t-1 <120829237+j-t-1@users.noreply.github.com>
2025-11-06 08:51:24 +0000

    Add type ignore

--- pypdf/_writer.py
+++ pypdf/_writer.py
@@ -2232,7 +2232,7 @@ class PdfWriter(PdfDocCommon):
 
             clean(content, images, forms, text_filters)
             page.replace_contents(content)
-        return [], []
+        return [], []  # type: ignore[return-value]
 
     def remove_images(
         self,
