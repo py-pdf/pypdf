7dfe2e1da9a6737dee3bf3f9ff8582c914675546
stefan6419846 <96178532+stefan6419846@users.noreply.github.com>
2026-02-16 14:42:35 +0100

    SEC: Detect cyclic references when accessing TreeObject.children

--- pypdf/generic/_data_structures.py
+++ pypdf/generic/_data_structures.py
@@ -693,10 +693,19 @@ class TreeObject(DictionaryObject):
             return
 
         child_ref = self[NameObject("/First")]
+        last = self[NameObject("/Last")]
         child = child_ref.get_object()
+        visited: set[int] = set()
         while True:
+            child_id = id(child)
+            if child_id in visited:
+                logger_warning(f"Detected cycle in outline structure for {child}", __name__)
+                return
+            visited.add(child_id)
+
             yield child
-            if child == self[NameObject("/Last")]:
+
+            if child == last:
                 return
             child_ref = child.get(NameObject("/Next"))  # type: ignore
             if is_null_or_none(child_ref):
