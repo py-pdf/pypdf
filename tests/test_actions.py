95120119faf5b7d2660082893c287467eb91a833
j-t-1 <120829237+j-t-1@users.noreply.github.com>
2026-02-18 11:38:52 +0000

    Fix coverage

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -134,6 +134,31 @@ def test_page_add_action(pdf_file_writer):
     page.delete_action("open")
     assert page.get(NameObject("/AA")) is None
 
+    # Add an open action with a pre-existing open action which has a Next key with a None value
+    page.add_action("open", JavaScript("app.alert('This is page ' + this.pageNum);"))
+    page[NameObject("/AA")][NameObject("/O")][NameObject("/Next")] = None
+    page.add_action("open", JavaScript("app.alert('This is page ' + this.pageNum);"))
+    expected = {
+        "/O": {
+            "/Type": "/Action",
+            "/Next": {
+                "/Type": "/Action",
+                "/Next": NullObject(),
+                "/S": "/JavaScript",
+                "/JS": "app.alert('This is page ' + this.pageNum);"
+            "/S": "/JavaScript",
+            "/JS": "app.alert('This is page ' + this.pageNum);"
+        }
+    }
+    assert page[NameObject("/AA")] == expected
+    page.delete_action("open")
+    assert page.get(NameObject("/AA")) is None
+
+
+
+
+
+
     # Add a close action without a pre-existing action dictionary
     page.add_action("close", JavaScript("app.alert('This is page ' + this.pageNum);"))
     expected = {
