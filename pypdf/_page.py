814b7eb35f6a85050a827fda615ff57b6d79dd9e
j-t-1 <120829237+j-t-1@users.noreply.github.com>
2026-02-18 15:55:39 +0000

    Reorder imports

--- pypdf/_page.py
+++ pypdf/_page.py
@@ -373,10 +373,10 @@ class ImageFile:
                 "It can be installed via 'pip install pypdf[image]'"
             )
 
-        from ._reader import PdfReader  # noqa: PLC0415
-
         # to prevent circular import
         from ._image_xobject import _xobj_to_image  # noqa: PLC0415
+        from ._reader import PdfReader  # noqa: PLC0415
+
         from .generic import DictionaryObject, PdfObject  # noqa: PLC0415
 
         if self.indirect_reference is None:
