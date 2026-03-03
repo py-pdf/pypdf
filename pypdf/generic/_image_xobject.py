6d0fa2fc0b6e54d9d6f61ffa52d057c8b5ff032f
j-t-1 <120829237+j-t-1@users.noreply.github.com>
2026-03-03 10:13:11 +0000

    MAINT: Move and rename _xobj_image_helpers.py (#3661)
    Move from top level to more appropriate generic folder. Also rename,
    to remove the redundant "helpers".

--- pypdf/generic/_image_xobject.py
+++ pypdf/generic/_image_xobject.py
@@ -4,12 +4,12 @@ import sys
 from io import BytesIO
 from typing import Any, Literal, Optional, Union, cast
 
-from ._utils import check_if_whitespace_only, logger_warning
-from .constants import ColorSpaces, StreamAttributes
-from .constants import FilterTypes as FT
-from .constants import ImageAttributes as IA
-from .errors import EmptyImageDataError, PdfReadError
-from .generic import (
+from .._utils import check_if_whitespace_only, logger_warning
+from ..constants import ColorSpaces, StreamAttributes
+from ..constants import FilterTypes as FT
+from ..constants import ImageAttributes as IA
+from ..errors import EmptyImageDataError, PdfReadError
+from ..generic import (
     ArrayObject,
     DecodedStreamObject,
     EncodedStreamObject,
