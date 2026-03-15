656f5fc5c9145c51d39760e589777b8404611aaa
github-action-benchmark <github@users.noreply.github.com>
2026-03-15 15:29:51 +0000

    add PyPy 3.11 Benchmark (pytest) benchmark result for 8f1f4aa8607a1e3b4730d67ca24e6b8f86bf526f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773588566772,
+  "lastUpdate": 1773588591155,
   "repoUrl": "https://github.com/py-pdf/pypdf",
   "entries": {
     "CPython Benchmark": [
@@ -96763,6 +96763,72 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.03709395182310544",
             "extra": "mean: 808.2704695999951 msec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "96178532+stefan6419846@users.noreply.github.com",
+            "name": "stefan6419846",
+            "username": "stefan6419846"
+          },
+          "committer": {
+            "email": "96178532+stefan6419846@users.noreply.github.com",
+            "name": "stefan6419846",
+            "username": "stefan6419846"
+          },
+          "distinct": true,
+          "id": "8f1f4aa8607a1e3b4730d67ca24e6b8f86bf526f",
+          "message": "REL: 6.9.0\n\n## What's new\n\n### New Features (ENH)\n- Expose /Perms verification result on Encryption object (#3672) by @costajohnt\n\n### Performance Improvements (PI)\n- Fix O(n²) performance in NameObject read/write (#3679) by @dmitry-kostin\n- Batch-parse all objects in ObjStm on first access (#3677) by @dmitry-kostin\n\n### Bug Fixes (BUG)\n- Avoid sharing array-based content streams between pages (#3681) by @stefan6419846\n- Avoid accessing invalid page when inserting blank page under some conditions (#3529) by @j-t-1\n\n[Full Changelog](https://github.com/py-pdf/pypdf/compare/6.8.0...6.9.0)",
+          "timestamp": "2026-03-15T16:26:40+01:00",
+          "tree_id": "0527ce2d11f14da9a2dd69a59c2aa2d9fd29aba7",
+          "url": "https://github.com/py-pdf/pypdf/commit/8f1f4aa8607a1e3b4730d67ca24e6b8f86bf526f"
+        },
+        "date": 1773588585421,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/bench.py::test_page_operations",
+            "value": 12.51932604449145,
+            "unit": "iter/sec",
+            "range": "stddev: 0.016915687791860343",
+            "extra": "mean: 79.87650425000344 msec\nrounds: 12"
+          },
+          {
+            "name": "tests/bench.py::test_merge",
+            "value": 24.445543707650117,
+            "unit": "iter/sec",
+            "range": "stddev: 0.011049827211799604",
+            "extra": "mean: 40.90725131579114 msec\nrounds: 19"
+          },
+          {
+            "name": "tests/bench.py::test_text_extraction",
+            "value": 1.1899793752203431,
+            "unit": "iter/sec",
+            "range": "stddev: 0.030286521725486286",
+            "extra": "mean: 840.3506992000047 msec\nrounds: 5"
+          },
+          {
+            "name": "tests/bench.py::test_read_string_from_stream_performance",
+            "value": 0.4256333872696587,
+            "unit": "iter/sec",
+            "range": "stddev: 0.14267977441696417",
+            "extra": "mean: 2.3494397523999995 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/bench.py::test_image_new_property_performance",
+            "value": 0.1990322148990773,
+            "unit": "iter/sec",
+            "range": "stddev: 0.05318921438937018",
+            "extra": "mean: 5.024312272799994 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/bench.py::test_large_compressed_image_performance",
+            "value": 1.325632841741304,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0020464571685716846",
+            "extra": "mean: 754.3566880000014 msec\nrounds: 5"
           }
         ]
       }
