49cf2402d89c8fc3779b1805ceddc63e3e6555e1
iso <iso.techdev@gmail.com>
2026-03-18 17:45:09 +0200

    ruff was commented out from the requirements

--- pypdf/generic/_image_xobject.py
+++ pypdf/generic/_image_xobject.py
@@ -181,7 +181,7 @@ def _handle_flate(
     size: tuple[int, int],
     data: bytes,
     mode: mode_str_type,
-    color_space: str | ArrayObject,
+    color_space: Union[str, ArrayObject],
     colors: int,
     obj_as_text: str,
 ) -> tuple[Image.Image, str, str, bool]:
