6461c2a8b1264755bffc72a8a0a88c93b8c70fb1
github-action-benchmark <github@users.noreply.github.com>
2026-04-10 19:11:09 +0000

    add CPython Benchmark (pytest) benchmark result for 500d09d92fa80a6f1fcdfa46656893efd05e91ff

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775814969655,
+  "lastUpdate": 1775848269641,
   "repoUrl": "https://github.com/py-pdf/pypdf",
   "entries": {
     "CPython Benchmark": [
@@ -93659,6 +93659,72 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.004313415765168212",
             "extra": "mean: 605.9791921999988 msec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mgorny@gentoo.org",
+            "name": "Michał Górny",
+            "username": "mgorny"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "500d09d92fa80a6f1fcdfa46656893efd05e91ff",
+          "message": "TST: Update `test_embedded_file__basic` to use `tmp_path` fixture (#3726)\n\nReplace the old `tmpdir` fixture with `tmp_path` in\n`test_embedded_file__basic` to fix the test collection error when `py`\nis installed:\n\n```pytb\n____________________________________________ ERROR collecting tests/generic/test_files.py _____________________________________________\n.venv/lib/python3.12/site-packages/py/_vendored_packages/apipkg/__init__.py:150: in __makeattr\n    modpath, attrname = self.__map__[name]\n                        ^^^^^^^^^^^^^^^^^^\nE   KeyError: 'LocalPath'\n\nDuring handling of the above exception, another exception occurred:\ntests/generic/test_files.py:31: in <module>\n    def test_embedded_file__basic(tmpdir: path.LocalPath) -> None:\n                                          ^^^^^^^^^^^^^^\n.venv/lib/python3.12/site-packages/py/_vendored_packages/apipkg/__init__.py:155: in __makeattr\n    raise AttributeError(name)\nE   AttributeError: LocalPath\n```\n\nSigned-off-by: Michał Górny <mgorny@gentoo.org>",
+          "timestamp": "2026-04-10T21:08:31+02:00",
+          "tree_id": "c28287a1ce0fb7ea17517e83a1368661db2186f7",
+          "url": "https://github.com/py-pdf/pypdf/commit/500d09d92fa80a6f1fcdfa46656893efd05e91ff"
+        },
+        "date": 1775848264640,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/bench.py::test_page_operations",
+            "value": 3.1293111130572653,
+            "unit": "iter/sec",
+            "range": "stddev: 0.009441859937532014",
+            "extra": "mean: 319.55915020000134 msec\nrounds: 5"
+          },
+          {
+            "name": "tests/bench.py::test_merge",
+            "value": 19.491860775411702,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0024599418760127742",
+            "extra": "mean: 51.303465150000704 msec\nrounds: 20"
+          },
+          {
+            "name": "tests/bench.py::test_text_extraction",
+            "value": 0.2853363612582749,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0357128731066821",
+            "extra": "mean: 3.5046357064000007 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/bench.py::test_read_string_from_stream_performance",
+            "value": 17.22954869141059,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0009682390675365492",
+            "extra": "mean: 58.0398255294132 msec\nrounds: 17"
+          },
+          {
+            "name": "tests/bench.py::test_image_new_property_performance",
+            "value": 0.08903596745734896,
+            "unit": "iter/sec",
+            "range": "stddev: 0.015671929305276652",
+            "extra": "mean: 11.231416118200002 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/bench.py::test_large_compressed_image_performance",
+            "value": 1.6396192662269427,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0021466471070224384",
+            "extra": "mean: 609.8976882000045 msec\nrounds: 5"
           }
         ]
       }
