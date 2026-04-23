8aff8eb7ad3fb681dc094a7f4b587ed4070f3042
j-t-1 <120829237+j-t-1@users.noreply.github.com>
2026-04-23 21:07:17 +0100

    MAINT: Increase readability of _merge_page

--- tests/test_page.py
+++ tests/test_page.py
@@ -842,11 +842,6 @@ def test_merge_page_with_annotations():
     pdf_path = RESOURCE_ROOT / "two-different-pages.pdf"
     writer = PdfWriter(clone_from=pdf_path)
 
-    annot0 = Polygon(
-        vertices=[(50, 550), (200, 650), (70, 750), (50, 700)],
-    )
-    writer.add_annotation(page_number=0, annotation=annot0)
-
     annot1 = Polygon(
         vertices=[(55, 555), (205, 655), (75, 755), (55, 705)],
     )
@@ -854,10 +849,8 @@ def test_merge_page_with_annotations():
 
     page0 = writer.pages[0]
     page1 = writer.pages[1]
-    assert len(page0.annotations) == 1
-    assert len(page1.annotations) == 1
     page0.merge_page(page1)
-    assert len(page0.annotations) == 2
+    assert len(page0.annotations) == 1
     assert len(page1.annotations) == 1
 
 
