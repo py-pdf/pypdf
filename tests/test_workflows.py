865ca85f56d0c4caa30839cc0fbf4bf078945b84
Stefan <96178532+stefan6419846@users.noreply.github.com>
2026-05-07 09:23:32 +0200

    add workaround for PyPy and Pillow

--- tests/test_workflows.py
+++ tests/test_workflows.py
@@ -5,6 +5,7 @@ They don't mock/patch anything, they cover typical user needs.
 """
 
 import binascii
+import platform
 from io import BytesIO
 from pathlib import Path
 from re import findall
@@ -14,6 +15,7 @@ from PIL import Image, ImageChops
 from PIL import __version__ as pil_version
 
 from pypdf import PdfReader, PdfWriter, Transformation
+from pypdf._utils import Version
 from pypdf.constants import PageAttributes as PG
 from pypdf.errors import PdfReadError, PdfReadWarning
 from pypdf.generic import (
@@ -955,7 +957,20 @@ def test_fields_returning_stream():
     assert "BtchIssQATit_time" in reader.get_form_text_fields()["TimeStampData"]
 
 
-def test_replace_image(tmp_path):
+def test_replace_image(tmp_path, monkeypatch):
+    from PIL import PdfParser  # noqa: PLC0415
+
+    def pdf_parser_close_buf(pdf_parser: PdfParser.PdfParser) -> None:
+        if isinstance(pdf_parser.buf, memoryview):
+            pdf_parser.buf.release()
+        elif isinstance(pdf_parser.buf, PdfParser.mmap.mmap):
+            pdf_parser.buf.close()
+        pdf_parser.buf = None
+
+    if Version(pil_version) < Version("12.3.0") and platform.python_implementation() == "PyPy":
+        # Regression since PyPy 7.3.22.
+        monkeypatch.setattr(PdfParser.PdfParser, "close_buf", pdf_parser_close_buf)
+
     writer = PdfWriter(clone_from=RESOURCE_ROOT / "labeled-edges-center-image.pdf")
     reader = PdfReader(RESOURCE_ROOT / "jpeg.pdf")
     img = reader.pages[0].images[0].image
