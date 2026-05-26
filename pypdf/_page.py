b1290a2cda779970ca523273ed16b4bb7c35a4ac
andreasantoro.pvt@gmail.com <andreasantoro.pvt@gmail.com>
2026-05-26 22:52:12 +0200

    remove docstrings in inline_images setter

--- pypdf/_page.py
+++ pypdf/_page.py
@@ -788,17 +788,6 @@ class PageObject(DictionaryObject):
 
     @inline_images.setter
     def inline_images(self, value: Optional[dict[str, ImageFile]]) -> None:
-        """
-        Setter for inline_images.
-
-        Setting to None clears the cache and forces recalculation on next access,
-        emulating the previous caching control mechanism. Setting to a dict merges
-        the values into the existing cache.
-
-        .. deprecated::
-            Use :attr:`images` and filter by :attr:`ImageFile.is_inline` instead.
-            This property will be removed in pypdf 7.0.
-        """
         if value is None:
             self._content_stream_images = None
         else:
