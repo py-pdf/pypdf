11a0f77b3bec7a3ae6e2755c81f9d5fbf6889783
metsw24-max <metsw24@gmail.com>
2026-08-03 18:34:18 +0530

    ROB: Guard empty link destination arrays during page transfer (#3896)
    ---------
    
    Signed-off-by: Sayed Kaif <metsw24@gmail.com>

--- pypdf/_writer.py
+++ pypdf/_writer.py
@@ -3026,7 +3026,8 @@ class PdfWriter(PdfDocCommon):
                         if str(d) in self.get_named_dest_root():
                             outlist.append(ano.clone(self).indirect_reference)
                     else:
-                        d = cast("ArrayObject", d)
+                        if not isinstance(d, ArrayObject) or not d:
+                            continue
                         p = self._get_cloned_page(d[0], pages, reader)
                         if p is not None:
                             anc = ano.clone(self, ignore_fields=("/Dest",))
@@ -3041,7 +3042,8 @@ class PdfWriter(PdfDocCommon):
                     if str(d) in self.get_named_dest_root():
                         outlist.append(ano.clone(self).indirect_reference)
                 else:
-                    d = cast("ArrayObject", d)
+                    if not isinstance(d, ArrayObject) or not d:
+                        continue
                     p = self._get_cloned_page(d[0], pages, reader)
                     if p is not None:
                         anc = ano.clone(self, ignore_fields=("/D",))
