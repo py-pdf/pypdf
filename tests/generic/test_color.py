0de7ffb7be22d270c084d68d64fa429670a9b610
PJBrs <pjbrs@floorenpj.nl>
2026-09-04 16:52:01 +0200

    Move Color test to right place.

--- tests/generic/test_color.py
+++ tests/generic/test_color.py
@@ -1,9 +1,11 @@
 """Test the pypdf.generic.color module"""
+from typing import cast
+
 from pypdf.generic import ArrayObject
-from pypdf.generic._color import Color
+from pypdf.generic._color import Color, RGBColor
 
 
-def test_color():
+def test_color() -> None:
     color = Color.from_tuple(ArrayObject([]))
     assert not color
 
@@ -11,7 +13,7 @@ def test_color():
     assert not color
 
     color = Color.from_tuple(ArrayObject([1, 0, 0]))
-    assert color.as_operator() == "1 0 0 rg"
+    assert cast(RGBColor, color).as_operator() == "1 0 0 rg"
 
     color = Color.from_tuple(ArrayObject([4, 5, 0]))
     assert not color
