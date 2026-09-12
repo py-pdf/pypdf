8f749e39d0d782c53f0d626eec7f81e1d7b6dc16
r-kamei <ryo.kamei@gmail.com>
2026-09-12 17:49:31 +0900

    TYP: Allow page reference or index for link target

--- pypdf/_writer.py
+++ pypdf/_writer.py
@@ -2555,12 +2555,15 @@ class PdfWriter(PdfDocCommon):
             and isinstance(destination, DictionaryObject)
             and "target_page_index" in destination
         ):
+            target_page_reference: Union[IndirectObject, NumberObject]
             target_page_index = cast(int, destination["target_page_index"])
             fit_type = cast(str, destination["fit"])
             # Work around the intermediate destination containing native Python
             # objects instead of PdfObject instances.
             fit_args = cast(
-                Sequence[float | Any | None], dict(destination)["fit_args"])
+                Sequence[float | Any | None],
+                dict(destination)["fit_args"]
+            )
 
             if 0 <= target_page_index < len(self.pages):
                 target_page_reference = cast(
