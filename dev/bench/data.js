2aa7c44ee5bcc0373dfb262a128a6eceb1311d3e
github-action-benchmark <github@users.noreply.github.com>
2026-09-17 15:17:37 +0000

    add CPython Benchmark (pytest) benchmark result for 095d5af1dacc6c5fcfe7cde1dca0d33e56c7810e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789658201568,
+  "lastUpdate": 1789658256953,
   "repoUrl": "https://github.com/py-pdf/pypdf",
   "entries": {
     "CPython Benchmark": [
@@ -112601,6 +112601,72 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.005662712235284945",
             "extra": "mean: 550.6754494000006 msec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "96178532+stefan6419846@users.noreply.github.com",
+            "name": "Stefan",
+            "username": "stefan6419846"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "095d5af1dacc6c5fcfe7cde1dca0d33e56c7810e",
+          "message": "ROB: Detect cycles in `_get_object_from_stream` (#4101)\n\nThis would previously raise a RecursionError and now returns a proper\ninternal exception.",
+          "timestamp": "2026-09-17T17:14:32+02:00",
+          "tree_id": "e02fcea46415b2f0b7d203d70f1ef2c6988c7e1f",
+          "url": "https://github.com/py-pdf/pypdf/commit/095d5af1dacc6c5fcfe7cde1dca0d33e56c7810e"
+        },
+        "date": 1789658246835,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/bench.py::test_page_operations",
+            "value": 2.815309443746491,
+            "unit": "iter/sec",
+            "range": "stddev: 0.01098904375526109",
+            "extra": "mean: 355.2007408000037 msec\nrounds: 5"
+          },
+          {
+            "name": "tests/bench.py::test_merge",
+            "value": 18.542143142615554,
+            "unit": "iter/sec",
+            "range": "stddev: 0.001964207787655741",
+            "extra": "mean: 53.931198368417945 msec\nrounds: 19"
+          },
+          {
+            "name": "tests/bench.py::test_text_extraction",
+            "value": 0.2500717743503153,
+            "unit": "iter/sec",
+            "range": "stddev: 0.026977931037722398",
+            "extra": "mean: 3.998851939999997 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/bench.py::test_read_string_from_stream_performance",
+            "value": 16.79306468950835,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0012127223644524535",
+            "extra": "mean: 59.54839205882181 msec\nrounds: 17"
+          },
+          {
+            "name": "tests/bench.py::test_image_new_property_performance",
+            "value": 0.07329539260330173,
+            "unit": "iter/sec",
+            "range": "stddev: 0.06880602653592553",
+            "extra": "mean: 13.643422382799994 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/bench.py::test_large_compressed_image_performance",
+            "value": 1.5038379481358841,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0036831132960718927",
+            "extra": "mean: 664.9652651999986 msec\nrounds: 5"
           }
         ]
       }
