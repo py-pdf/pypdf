1264cf7dec6aba575ac75ff6cecb8ff8ebf0d3d8
mTangtrongwanit <MTangtrongwanit@esri.com>
2026-09-24 17:05:56 -0400

    TST: Refactor tests and output to tmp_path

--- tests/test_ocg_append.py
+++ tests/test_ocg_append.py
@@ -4,19 +4,16 @@ from pypdf import PdfReader, PdfWriter
 from tests import RESOURCE_ROOT, SAMPLE_ROOT
 
 
-def test_ocg():
+def test_ocg_append_metadata_preservation(tmp_path):
     # Trying to get OCGs to append while preserving the OCGs in the original document
-    reader = PdfReader(RESOURCE_ROOT / "Seige_of_Vicksburg_Sample_OCR.pdf")
-    writer = PdfWriter()
-    # writer.append(reader)
-    writer.clone_document_from_reader(reader)
+    writer = PdfWriter(clone_from=RESOURCE_ROOT / "Seige_of_Vicksburg_Sample_OCR.pdf")
+
     reader_append = PdfReader(SAMPLE_ROOT / "029-optional-content-groups/ocg_map_test.pdf")
 
-    # Trigger the append with the modified merge code that include the OCGs
+    # 1. Trigger the append with the modified merge code that include the OCGs
     writer.append(reader_append)
 
     # 2. Add or change metadata values
-    # Note: Keys must start with a forward slash (/)
     writer.add_metadata({
         "/Author": "Test Author",
         "/Producer": "Test Producer",
@@ -27,17 +24,40 @@ def test_ocg():
         "/CustomField": "Test CustomField",
     })
 
-    writer.write(RESOURCE_ROOT / "output-ocg_map_test-1.pdf")
+    writer.write(tmp_path / "output-ocg_append_metadata_preservation.pdf")
 
-def test_ocg_2():
-    # Trying to get OCGs to append while preserving the OCGs in the original document
-    reader = PdfReader(SAMPLE_ROOT / "029-optional-content-groups/ocg_map_test.pdf")
-    writer = PdfWriter()
-    # writer.append(reader)
-    writer.clone_document_from_reader(reader)
+def test_ocg_append_metadata_reverse_order(tmp_path):
+    # Testing the same scenario, but PDFs appended in reverse order
+    writer = PdfWriter(clone_from=SAMPLE_ROOT / "029-optional-content-groups/ocg_map_test.pdf")
+
     reader_append = PdfReader(RESOURCE_ROOT / "Seige_of_Vicksburg_Sample_OCR.pdf")
 
     # Trigger the append with the modified merge code that include the OCGs
     writer.append(reader_append)
 
-    writer.write(RESOURCE_ROOT / "output-ocg_map_test-2.pdf")
+    # 2. Add or change metadata values
+    writer.add_metadata({
+        "/Author": "Test Author",
+        "/Producer": "Test Producer",
+        "/Title": "Test Title",
+        "/Subject": "Test Subject",
+        "/Keywords": "Test Keywords",
+        "/Creator": "Test Creator",
+        "/CustomField": "Test CustomField",
+    })
+
+    writer.write(tmp_path / "output-ocg_append_metadata_reverse_order.pdf")
+
+def test_ocg_insert_with_merge(tmp_path):
+    # This test is meant to output a PDF that can be compared with the test_workflows.py
+    # "test_merge_output" test of expected vs actual size.
+    # Should be named "Seige_of_Vicksburg_Sample_OCR-crazyones-merged.pdf" in the Resource root
+    # or https://github.com/py-pdf/pypdf/blob/main/resources/Seige_of_Vicksburg_Sample_OCR-crazyones-merged.pdf
+    writer = PdfWriter(clone_from=RESOURCE_ROOT / "Seige_of_Vicksburg_Sample_OCR.pdf")
+
+    reader_append = PdfReader(RESOURCE_ROOT / "crazyones.pdf")
+
+    # Trigger the append with the modified merge code that include the OCGs
+    writer.merge(1, reader_append)
+
+    writer.write(tmp_path / "output-ocg_insert_with_merge.pdf")
